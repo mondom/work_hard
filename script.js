d1c429f3ac995731bf9ee7d5aace7af1bde520c2
Monika <codingbymon@gmail.com>
2024-04-19 10:17:54 +0200

    1 task

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+const btn = document.querySelector('.btn-start')
+
+const start = () => {
+	const liczbaPierwsza = 10
+	const liczbaDruga = 20
+
+    const suma = liczbaPierwsza + liczbaDruga
+    alert(suma)
+}
+
+start()
