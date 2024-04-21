da809ea790f419ff0b4f9dc4773c943bddb0a0ef
Monika <codingbymon@gmail.com>
2024-04-21 08:27:16 +0200

    finish task

--- script.js
+++ script.js
@@ -1,54 +1,24 @@
-'use strict'
+const favoritePlace = 'Góry'
+let isAvailable = false
 
-const students = [
-	{
-		name: 'Jan',
-		surname: 'Kowalski',
-		age: 21,
-		index: '12345',
-		grades: [4, 5, 3, 2],
-	},
-	{
-		name: 'Anna',
-		surname: 'Nowak',
-		age: 22,
-		index: '23456',
-		grades: [5, 5, 5, 4],
-	},
-	{
-		name: 'Piotr',
-		surname: 'Wisniewski',
-		age: 20,
-		index: '34567',
-		grades: [3, 3, 2, 2],
-	},
-]
+const availablePlace = 'Góry'
 
-const calculateAverageGrade = students => {
-	let averageGrades = {}
-
-	for (const studentGrades of students) {
-		const average =
-			studentGrades.grades.reduce(function (a, b) {
-				return a + b
-			}, 0) / studentGrades.grades.length
-		const surname = studentGrades.surname
-		averageGrades[surname] = average
-	}
-	const sumAll = Number((Object.values(averageGrades).reduce((a, b) => a + b, 0) / Object.keys(averageGrades).length).toFixed(2))
-
-
-	console.log(sumAll)
-
-	// students.map(obj => {
-	// 	const studentName = obj.name
-	// 	const value =
-	// 		obj.grades.reduce(function (accumulator, currentValue) {
-	// 			return accumulator + currentValue
-	// 		}, 0) / obj.grades.length
-
-	//         averageGrades[studentName] = value
-	// })
+if(favoritePlace === availablePlace){
+    isAvailable = true;
 }
 
-calculateAverageGrade(students)
+if(isAvailable) {
+    console.log(`Moje ulubione miejsce wypoczynkowe jest dostępne!`);
+}else{
+    console.log(`Moje ulubione miejsce wypoczynkowe nie jest dostępne!`);
+}
+
+if(!favoritePlace){
+    console.log(`Błąd! Nie podano ulubionego miejsca!`);
+}
+
+const secretKey = Symbol('klucz dostępu')
+
+if(favoritePlace === secretKey){
+    console.log(`Błąd! Nie masz dostepu!`);
+}
