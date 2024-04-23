b5986787f4d54310ffea15896e708fcd0a430489
Monika <codingbymon@gmail.com>
2024-04-23 16:26:01 +0200

    finish taks

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 'use strict'
 const test = () => {
-
-const sentence = 'to jest przykladowe zdanie.'
-console.log(sentence.length);
-console.log(sentence[0]);
-console.log(sentence[sentence.length -1]);
-console.log(sentence.includes('to'));
-const modify = sentence[0].toUpperCase() + sentence.slice(1)
-console.log(modify);
+	const sentence = 'Chcę uzyskać kota z tego zdania.'
+	const lastIndex = sentence.length - 1
+	console.log(sentence[lastIndex])
+	console.log(sentence)
+	const frag1 = sentence.slice(13, 16)
+	console.log(frag1)
+	const frag2 = sentence.substring(13, 16)
+	console.log(frag2)
+	const frag3 = sentence.substr(13, 3)
+	console.log(frag3)
 }
 
 test()
-
