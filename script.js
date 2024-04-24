9b77d26b2629b34f1c35fa909ac358467121fb39
Monika <codingbymon@gmail.com>
2024-04-24 12:42:51 +0200

    finish excercise

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 'use strict'
-const test = () => {
-	const year = parseInt(prompt('Podaj który mamy rok'))
-	let isLeapYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
 
-	if (year <= 2001 || year >= 2100) {
-		console.log(`${year} nie należy do XXI wieku`)
-	} else if (!isLeapYear && (year >= 2001 && year <= 2100)) {
-		console.log(`${year} nie jest przestepny i należy do XXI wieku.`)
-	} else if (isLeapYear && (year >= 2001 && year <= 2100)) {
-		console.log(`${year} jest przestepny i należy do XXI wieku.`)
-	}
+const getValue = (optionalValue, defaultValue) => {
+	return optionalValue ?? defaultValue
 }
 
-test()
+let value = getValue('wartość opcjonalna', 'wartość domyślna')
+console.log(value);
+
+
