a216c30cb56d6e8f6e73a4c219f5980db18bf55b
Monika <codingbymon@gmail.com>
2024-04-24 09:24:43 +0200

    finish next level

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 'use strict'
 const test = () => {
-	const sentence = 'Chcę uzyskać kota z tego zdania.'
-	const lastIndex = sentence.length - 1
-	console.log(sentence[lastIndex])
-	console.log(sentence)
-	const frag1 = sentence.slice(13, 16)
-	console.log(frag1)
-	const frag2 = sentence.substring(13, 16)
-	console.log(frag2)
-	const frag3 = sentence.substr(13, 3)
-	console.log(frag3)
-}
+	const minHeight = 150
+	const userHeight = prompt('Podaj swój wzrost w cm')
+
+	if (userHeight === '' || isNaN(userHeight)) {
+		console.log('podaj wzrost wpisując liczbę')
+	} else if (userHeight >= minHeight) {
+		console.log('Możesz skorzystać z karuzeli')
+	} else {
+		console.log('Jesteś za niski');
+	}
+} 
 
 test()
