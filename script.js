250f6530c4baafb72fa780fa2fbe07cae1fed338
Monika <codingbymon@gmail.com>
2024-04-24 13:16:01 +0200

    finish excercise

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 'use strict'
 
-const getValue = (optionalValue, defaultValue) => {
-	return optionalValue ?? defaultValue
+function test() {
+	let price = 3200
+	let isRegularCustomer = true
+	let numberOfPurchases = 3
+	numberOfPurchases = numberOfPurchases ?? 0
+	
+	let discount = 10 
+	discount = discount ?? 0
+	
+	let totalPrice
+
+	if (isRegularCustomer && numberOfPurchases === 3) {
+		totalPrice = price - (price * discount / 100)
+	}
+
+	console.log(`Cena roweru: ${totalPrice} pln`)
 }
 
-let value = getValue('wartość opcjonalna', 'wartość domyślna')
-console.log(value);
-
-
+test()
