068d680fc48aba682d0c6cb44fd8c79bf3bc1173
Monika <codingbymon@gmail.com>
2024-04-25 11:33:48 +0200

    loop while

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 'use strict'
 
 function test() {
-	let price = 3200
-	let isRegularCustomer = true
-	let numberOfPurchases = 3
-	numberOfPurchases = numberOfPurchases ?? 0
-	
-	let discount = 10 
-	discount = discount ?? 0
-	
-	let totalPrice
+	let sumaKalorii = 0
 
-	if (isRegularCustomer && numberOfPurchases === 3) {
-		totalPrice = price - (price * discount / 100)
+	let wprowadzoneKalorie
+	while (wprowadzoneKalorie !== 'stop') {
+		wprowadzoneKalorie = prompt('Wpisz ilość spożytych kalorii')
+		if (wprowadzoneKalorie !== 'stop') {
+			sumaKalorii = parseInt(sumaKalorii) + parseInt(wprowadzoneKalorie)
+		}
 	}
-
-	console.log(`Cena roweru: ${totalPrice} pln`)
+	alert(sumaKalorii)
 }
 
 test()
