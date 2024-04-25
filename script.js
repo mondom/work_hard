b98163077fe8f537bc5e0e4b107bde0a9392aaa8
Monika <codingbymon@gmail.com>
2024-04-25 15:56:09 +0200

    bmi

--- script.js
+++ script.js
@@ -2,24 +2,9 @@
 const input = document.querySelector('input')
 const btn = document.querySelector('.btn')
 
-const grades = [3, 4, 5, 2, 3, 5, 4, 2, 4, 5]
-// suma ocen
-let sum = 0
-// liczba ocen branych pod uwagę
-let count = 0
-
-function test() {
-	for (let i = 0; i < grades.length; i++) {
-		if (grades[i] < 3) {
-			continue
-		} else {
-			sum = sum + grades[i]
-			count++
-		}
-	}
-    const average = sum /count
-    console.log(`Średnia ocen to: ${average.toFixed(2)}`);
+function calculateBMI(height, weight) {
+	return weight / (height * height)
 }
-test()
 
-btn.addEventListener('click', test)
+const bmiResult = calculateBMI(1.8, 66)
+console.log(bmiResult.toFixed(2))
