ad5e544ffe7176405219902d6ef01e5f1d202fa1
Monika <codingbymon@gmail.com>
2024-05-06 12:36:02 +0200

    arr

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 'use strict'
-const input = document.querySelector('input')
-const btn = document.querySelector('.btn')
 
 const products = [
-	{ name: 'Koszula', price: 49.99 },
-	{ name: 'Spodnie', price: 79.99 },
-	{ name: 'Buty', price: 129.99 },
-	{ name: 'Kurtka', price: 149.99 },
+	{name: "koszula",
+	 price: 49.99
+	}
+	{name: "spodnie",
+	 price: 79.99
+	}
+	{name: "buty",
+	 price: 129.99
+	}
+
 ]
-
-const filterProducts = filterFunction => {
-	const productsFilter = products.filter(filterFunction)
-	return productsFilter
-}
-
-const filteredProducts = filterProducts(product => product.price < 100)
-
-console.log(filteredProducts)
