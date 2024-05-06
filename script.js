a2ce13c9eddc10ca815b54d2ea90257e3b4274e2
Monika <codingbymon@gmail.com>
2024-05-06 12:43:06 +0200

    first steps

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 'use strict'
 
 const products = [
-	{name: "koszula",
-	 price: 49.99
-	}
-	{name: "spodnie",
-	 price: 79.99
-	}
-	{name: "buty",
-	 price: 129.99
-	}
-
+	{ name: 'koszula', price: 49.99 },
+	{ name: 'spodnie', price: 79.99 },
+	{ name: 'buty', price: 129.99 },
 ]
+
+const filterProducts = filterFunction => {
+	const filteredProducts = products.filter(filterFunction)
+	 console.log(filteredProducts);
+}
+
+const filterFunction = () => {
+	products.price < 100
+}
+filterProducts(filterFunction)
