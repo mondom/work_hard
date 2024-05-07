510df93418f3a4f09ef82b932dc2e89733e257e4
Monika <codingbymon@gmail.com>
2024-05-07 19:47:10 +0200

    test2

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ const products = [
 	{ name: 'koszula', price: 49.99 },
 	{ name: 'spodnie', price: 79.99 },
 	{ name: 'buty', price: 129.99 },
+	{ name: "Kurtka", price: 149.99 }
 ]
 
 const filterProducts = filterFunction => {
 	const filteredProducts = products.filter(filterFunction)
-	 console.log(filteredProducts);
+	 return filteredProducts
 }
 
-const filterFunction = () => {
-	products.price < 100 
-}
-filterProducts(filterFunction)
+const result = filterProducts((product) => {
+	product.price < 100
+})
+console.log(result[0].name + ": " + result[0].price);
+console.log(result[1].name + ": " + result[1].price);
+console.log(result);
+
+// const filterFunction = () => {
+// 	const test = products.price < 100 
+// 	return test
+// }
+// filterProducts(filterFunction)
+// filterFunction()
