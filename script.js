e5d36df9aa625854aa45c0755adfaebaffb1a3f8
Monika <codingbymon@gmail.com>
2024-05-09 20:04:52 +0200

    player

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 'use strict'
 
-function Task(title,description){
-	this.title = title
-	this.description = description
+const Player = function (nick, email) {
+	// konstruktor
+	this.nick = nick
+	this.email = email
+	this.shoot = () => console.log('SHOOT!!!')
 }
 
-Task.prototype.printTask = function(){
-	console.log(`Zadanie: ${this.title}`);
-	console.log(`Opis: ${this.description}`);
-}
+Player.prototype.login = () => console.log('Jestem zalogowany!')
+Player.prototype.logout = () => console.log('Jestem wylogowany!')
+Player.prototype.moveLeft = () => console.log('Idę w lewo!')
+Player.prototype.moveRight = () => console.log('Idę w prawo!')
 
-const task1 = new Task('Zakupy spożywcze', 'Kupić jajka, mleko i chleb')
-
-task1.printTask();
+const Player1 = new Player('Dragon', 'janek@example.com')
+const Player2 = new Player('Fenix', 'john@example.com')
+const Player3 = new Player('Kmaikadze', 'tom@example.com')
+Player1.login()
