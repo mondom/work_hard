22a30015f8e21f11d4da0690ead785af9be49aa5
Monika <codingbymon@gmail.com>
2024-05-09 12:17:01 +0200

    course excercise

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 'use strict'
 
-function Person(name, age, hobby) {
-	this.name = name
-	this.age = age
-	this.hobby = hobby
+function Person() {
+	let self = this
+	self.name = ''
+	self.setName = function (name) {
+		self.name = name
+	}
 }
 
-Person.prototype.hello = function(){
-	console.log(`hello ${this.name}`);
-}
-
-const person1 = new Person('Monika', 34, 'siatkówka')
-const person2 = new Person('Łukasz', 42, 'zarządzanie')
-const person3 = new Person('Lucjan', 6, 'spanko')
-
-console.log(person1)
-console.log(person2)
-console.log(person3)
-person1.hello()
-person2.hello()
-person3.hello()
+const person = new Person()
+person.setName('Antoni')
+console.log(person.name);
