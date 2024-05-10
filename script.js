24fb5efb5606848f26e74ef3904b8c7dc2c70569
Monika <codingbymon@gmail.com>
2024-05-10 11:38:42 +0200

    article

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 'use strict'
 
-const Player = function (nick, email) {
-	// konstruktor
-	this.nick = nick
-	this.email = email
-	this.shoot = () => console.log('SHOOT!!!')
-}
-
-Player.prototype.login = () => console.log('Jestem zalogowany!')
-Player.prototype.logout = () => console.log('Jestem wylogowany!')
-Player.prototype.moveLeft = () => console.log('Idę w lewo!')
-Player.prototype.moveRight = () => console.log('Idę w prawo!')
-
-const Player1 = new Player('Dragon', 'janek@example.com')
-const Player2 = new Player('Fenix', 'john@example.com')
-const Player3 = new Player('Kmaikadze', 'tom@example.com')
-console.log(Player1);
+let PlayerProto = {
+	login: () => console.log("Jestem zalogowany!"),
+	logout: () => console.log("Jestem wylogowany!"),
+  };
+   
+  // tylko prototyp
+  let Player4 = Object.create(PlayerProto);
+  Player4.nick = "AngryBird";
+  Player4.mail = "max@example.com";
+   
+  // prototyp plus property - musimy podawać to w sposób { property: {value: "wartość property"} }
+  let Player5 = Object.create(PlayerProto, {
+	nick: { value: "Joker" },
+	email: { value: "andrew@example.com" },
+  });
+  console.log(Player4);
+  console.log(Player5);
