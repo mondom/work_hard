e28aa560896c1c8d01bea045f955f141460b6a2d
Monika <codingbymon@gmail.com>
2024-05-11 08:12:17 +0200

    constructor excercise

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 'use strict'
 
 class Vegetable {
-	constructor(name,color){
+	constructor(name, color) {
 		this.name = name
 		this.color = color
 	}
 
-	description(){
-		console.log(`${this.name} have ${this.color} color`);
+	description() {
+		console.log(`${this.name} have ${this.color} color`)
 	}
 }
 
-const potato = new Vegetable('potato', 'red')
-console.log(potato);
+const potato = new Vegetable('Potato', 'red')
+console.log(potato)
 potato.description()
+
+class Fruit extends Vegetable {
+	constructor(name, color, size) {
+		super(name, color)
+		this.size = size
+	}
+	description(){
+		console.log(`${this.name} have ${this.color} color and is very ${this.size}`)
+	}
+}
+
+const apple = new Fruit('Apple', 'green', 'small')
+console.log(apple)
+apple.description()
