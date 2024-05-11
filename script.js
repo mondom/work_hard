9f1981d73b11f95b014bc21dd96455306488f80f
Monika <codingbymon@gmail.com>
2024-05-11 09:26:08 +0200

    course

--- script.js
+++ script.js
@@ -1,49 +1,19 @@
 'use strict'
 
-class Player {
-	constructor(nick, mail) {
-		this.nick = nick
-		this.mail = mail
-	}
-	shoot() {
-		console.log('SHOOT!!!')
-	}
-	login() {
-		console.log('Jestem zalogowany!')
-	}
-	logout() {
-		console.log('Jestem wylogowany!')
-	}
-	moveLeft() {
-		console.log('Idę w lewo!')
-	}
-	moveRight() {
-		console.log('Idę w prawo!')
+const userPrototype = {
+	introduce(){
+		console.log(`Hi, my name is ${this.name}. I am ${this.age} years old.`);
 	}
 }
 
-const Player1 = new Player('Dragon', 'janek@example.com')
-const Player2 = new Player('Fenix', 'john@example.com')
-const Player3 = new Player('Kmaikadze', 'tom@example.com')
+function createUser(name, age){
+	const user = Object.create(userPrototype)
+	user.name = name
+	user.age = age
+	return user
 
-console.log(Player1);
-Player1.shoot()
-
-class Leader extends Player{
-	constructor(nick,mail,team){
-		super(nick,mail)
-		this.team = team
-	}
-	jump(){
-		console.log('Mogę skakać!');
-	}
-	invite(nick){
-		console.log(`${nick}, zapraszam Cię do mojego zespołu`);
-	}
 }
 
-const vip = new Leader('Pablo','pablo@vp.pl','gladiators')
+const user1 = createUser('john', 25)
+user1.introduce()
 
-console.log(vip);
-vip.invite('Boo')
-vip.jump()
