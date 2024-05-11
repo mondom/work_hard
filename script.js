d5bf957e7896f92632a71d58429566f3943f04eb
Monika <codingbymon@gmail.com>
2024-05-11 07:57:34 +0200

    constructor

--- script.js
+++ script.js
@@ -1,48 +1,16 @@
 'use strict'
 
-class Person2 {
-    constructor(name, age) {
-        this.name = name
-        this.age = age
-    }
-
-    sayHi() {
-        console.log(`Hi, I'm ${this.name}`)
-    } 
-
-    showAge() {
-        console.log(`I'm ${this.age} years old`)
-    }
-}
-
-// =================================
-
-
-const person2 = new Person2('Łukasz', 42)
-
-console.log(person2);
-
-person2.sayHi()
-person2.showAge()
-
-
-class Cat {
-	constructor(name,age){
+class Vegetable {
+	constructor(name,color){
 		this.name = name
-		this.age = age
+		this.color = color
 	}
 
-	sayHi(){
-		console.log(`Hello ${this.name}`);
-	}
-	sayYears(){
-		console.log(`You are ${this.age} years old`);
+	description(){
+		console.log(`${this.name} have ${this.color} color`);
 	}
 }
 
-const Lucjan = new Cat('Lucjan', 6)
-
-console.log(Lucjan);
-
-Lucjan.sayHi()
-Lucjan.sayYears()
+const potato = new Vegetable('potato', 'red')
+console.log(potato);
+potato.description()
