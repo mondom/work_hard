2453ac6a3f0fce763b9d27f82d33a350d05adec3
Monika <codingbymon@gmail.com>
2024-05-12 14:22:10 +0200

    fault finding

--- script.js
+++ script.js
@@ -3,21 +3,18 @@
 let products = [
 	{ name: 'Product 1', price: 10.99, quantity: 50 },
 	{ name: 'Product 2', price: 5.99, quantity: 20 },
-	{ name: 'Product 3', price: 15.99, quantity: 10 }
+	{ name: 'Product 3', price: 15.99, quantity: 10 },
 ]
 
 products.forEach(product => {
-	console.log(`Product name: ${product.name}`);
-	console.log(`Product price: ${product.price}`);
-	console.log(`Product quantity: ${product.quantity}`);
+	console.log(`Product name: ${product.name}`)
+	console.log(`Product price: ${product.price}`)
+	console.log(`Product quantity: ${product.quantity}`)
 })
 
-const product3 = products.find(product => {
-	product.name === 'Product 3'
-	
-})
-product3.quantity = 5 + product3.quantity;
 
-// const product3 = products.find((product) => product.name === "Product 3");
-// product3.quantity += 5;
-// console.log("New quantity of Product 3:", product3.quantity);
+
+const product3 = products.find(product => product.name === 'Product 3')
+product3.quantity = 5 + product3.quantity
+console.log(product3)
+
