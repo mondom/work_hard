0feca9295904fb707dc9bbccb3c9a0ffcdc1beab
Monika <codingbymon@gmail.com>
2024-05-15 16:40:48 +0200

    tests

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const tasks = [
 		name: 'Sprzatanie mieszkania',
 		completed: true,
 		priority: 2,
-		dueDate: '2024-05-15',
+		dueDate: '2024-01-15',
 		description: 'Odkurzyć i umyć podłogi',
 		category: 'domowe',
 	},
@@ -21,7 +21,7 @@ const tasks = [
 		name: 'Nauka JavaScript',
 		completed: false,
 		priority: 3,
-		dueDate: '2024-06-01',
+		dueDate: '2024-05-15',
 		description: 'Przerobić rozdziały 1-3',
 		category: 'nauka',
 	},
@@ -34,16 +34,30 @@ const tasks = [
 // }
 
 // manageTasks(tasks)
-
+let taskDate
+let fullDate
 const inTime = tasks => {
-	for (const task of tasks) {
-		console.log(task.dueDate)
-	}
 	const today = new Date()
 	const year = today.getFullYear()
 	const month = String(today.getMonth() + 1).padStart(2, '0')
 	const day = String(today.getDate()).padStart(2, '0')
-	const fullDate = `${year}-${month}-${day}`
-	console.log(fullDate)
+	fullDate = `${year}-${month}-${day}`
+	const newFullDate = new Date(fullDate)
+	console.log(newFullDate.getTime())
+
+	for (const task of tasks) {
+		taskDate = task.dueDate
+		// console.log(taskDate);
+
+		if (taskDate === fullDate) {
+			console.log(`Zadanie jest na dzisiaj!`)
+		} else if (taskDate < fullDate) {
+			console.log(`Zadanie jest po terminie...`)
+		} else if (taskDate > fullDate) {
+			console.log(`Masz jeszcze czas na wykonanie zadania :)`)
+		}
+	}
+
+	// console.log(fullDate)
 }
 inTime(tasks)
