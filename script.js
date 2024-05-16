f93a8ef277a4e1f65012a5f1969ccfd50815dbc4
Monika <codingbymon@gmail.com>
2024-05-16 15:28:50 +0200

    learning

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
 'use strict'
 
-const objects = [
-	{ name: 'object1' },
-	{ name: 'object2' },
-	{ name: 'object3' }
-  ];
+const objects = [{ name: 'object1' }, { name: 'object2' }, { name: 'object3' }]
 
-  // Definiowanie węzłów
-const node1 = { value: 'kotek', next: null };
-const node2 = { value: 'piesek', next: null };
-const node3 = { value: 'pszczółka', next: null };
-
-// Łączenie węzłów w listę
-node1.next = node2;
-node2.next = node3;
-
-// Teraz node1 to głowa listy jednokierunkowej
-let currentNode = node1;
-while (currentNode !== null) {
-    console.log(currentNode.value);
-    currentNode = currentNode.next;
+const osoba = {
+	name: 'Ja',
+	rodzice: [
+		{
+			name: 'Ojciec',
+			rodzice: [
+				{ name: 'Dziadek', rodzice: [] },
+				{ name: 'Babcia', rodzice: [] },
+			],
+		},
+		{
+			name: 'Matka',
+			rodzice: [
+				{ name: 'Dziadek', rodzice: [] },
+				{ name: 'Babcia', rodzice: [] },
+			],
+		},
+	],
 }
 
+const policzPokolenia = osoba => {
+	if (osoba.rodzice.length === 0) {
+		return 1 // Podstawowy przypadek: brak rodziców
+	} else {
+		let maxPokoleniaRodzicow = 0
+		osoba.rodzice.forEach(rodzic => {
+			const pokoleniaRodzica = policzPokolenia(rodzic)
+			if (pokoleniaRodzica > maxPokoleniaRodzicow) {
+				maxPokoleniaRodzicow = pokoleniaRodzica
+			}
+		})
+		return 1 + maxPokoleniaRodzicow
+	}
+}
+
+console.log(policzPokolenia(osoba)) // Wynik: 3
+
 // Output:
-// 1
-// 2
-// 3
+// root
+// child1
+// grandchild1
+// child2
