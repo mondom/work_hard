42b07a99080f0ffb747c92699d864d53417d7e65
Monika <codingbymon@gmail.com>
2024-05-17 15:50:58 +0200

    new test

--- script.js
+++ script.js
@@ -1,47 +1,41 @@
 'use strict'
 
-// const objects = [
-// 	{ name: 'object1' },
-// 	{ name: 'object2' },
-// 	{ name: 'object3' }
-//   ];
-
-// objects[0].ref = objects[1]
-// objects[1].ref = objects[2]
-// console.log(objects[0]);
-// console.log(objects[1]);
-
-// function garbageCollage(objects){
-// const reachableObjects = new Set()
-// console.log(reachableObjects);
-// }
-
-// garbageCollage(objects)
-
-const objects = [
-	{ name: 'object1' }, 
-	{ name: 'object2' }, 
-	{ name: 'object3' }
-]
-
-objects[0].ref = objects[1]
-objects[1].ref = objects[2]
-
-const stack = [objects[0]]
-const visited = new Set()
-
-while (stack.length > 0) {
-	const currentObject = stack.pop()
-
-	if (visited.has(currentObject)) {
-		continue
-	}
-
-	visited.add(currentObject)
-
-	console.log(currentObject.name)
-
-	if (typeof currentObject.ref !== 'undefined') {
-		stack.push(currentObject.ref)
-	}
+const objects = [{ name: "object1" }, { name: "object2" }, { name: "object3" }];
+ 
+objects[0].ref = objects[1];
+objects[1].ref = objects[2];
+ 
+function garbageCollect(objects) {
+    const reachableObjects = new Set();
+    const stack = [objects[0]];
+ 
+    while (stack.length > 0) {
+        const currentObj = stack.pop();
+        reachableObjects.add(currentObj);
+ 
+        for (const key in currentObj) {
+            if (
+                typeof currentObj[key] === "object" &&
+                currentObj[key] !== null &&
+                !reachableObjects.has(currentObj[key])
+            ) {
+                stack.push(currentObj[key]);
+            }
+        }
+    }
+ 
+    for (let i = 0; i < objects.length; i++) {
+        if (!reachableObjects.has(objects[i])) {
+            delete objects[i];
+        }
+    }
+ 
+    console.log("Pozostałe obiekty:");
+    for (const obj of objects) {
+        if (obj !== undefined) {
+            console.table(obj);
+        }
+    }
 }
+ 
+garbageCollect(objects);
