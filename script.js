b6d9aa58d355e18c5c5cbf9901dbbbd6ef9169ff
Monika <codingbymon@gmail.com>
2024-05-19 08:49:33 +0200

    optional chaining operator

--- script.js
+++ script.js
@@ -1,63 +1,18 @@
 'use strict'
 
-// const playlist = {
-// 	currenrSongIndex: 0,
-// 	songs: [],
-// 	addSong(name) {
-// 		this.songs.push(name)
-// 	},
-// 	showPlaylist() {
-// 		for (const song of this.songs) {
-// 			console.log(song)
-// 		}
-// 	},
-// 	playCurrentSong() {
-// 		const currentSong = this.songs[this.currenrSongIndex]
-// 		console.log(`odtwarzam: ${currentSong}`)
-// 		this.currenrSongIndex++
-// 		if (this.currenrSongIndex >= this.songs.length) {
-// 			this.currenrSongIndex = 0
-// 		}
-// 		setTimeout(() => {
-// 			this.playCurrentSong()
-// 		}, 10000)
-// 	},
-// }
-
-// playlist.addSong('NF - HOPE')
-// playlist.addSong('Perfect - Autobiografia')
-// playlist.addSong('Imagine Dragons - Wrecked')
-
-// playlist.showPlaylist()
-// playlist.playCurrentSong()
-
-const playList = {
-	currentIndex: 0,
-	songs: [],
-	addSong(name) {
-		this.songs.push(name)
-	},
-	showList() {
-		this.songs.forEach(song => {
-			console.log(song)
-		})
-	},
-	playSongs() {
-		const currentSong = this.songs[this.currentIndex]
-		console.log(`Teraz leci: ${currentSong}`)
-		this.currentIndex++
-		if (this.currentIndex >= this.songs.length) {
-			this.currentIndex = 0
-		}
-		setTimeout(() => {
-			this.playSongs()
-		}, 5000)
-	},
+const user= {
+	name: 'John Doe',
+	address: {
+		street: 'Brodway',
+		city: 'New York',
+		country: 'USA'
+	}
 }
 
-playList.addSong("Beyoncé - TEXAS HOLD'EM")
-playList.addSong('Sanah - Hip Hip Hurra')
-playList.addSong('Rihanna - Stay')
-playList.addSong("Fergie - Big Girls Don't Cry")
-playList.showList()
-playList.playSongs()
+console.log(user.address.country);
+console.log(user.address?.country);
+
+
+
+
+
