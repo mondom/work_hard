715c55eb4e507dc50699f6a81374f565b6298410
Monika <codingbymon@gmail.com>
2024-05-19 08:15:55 +0200

    my try

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
 'use strict'
 
-const playlist = {
-	currenrSongIndex: 0,
+// const playlist = {
+// 	currenrSongIndex: 0,
+// 	songs: [],
+// 	addSong(name) {
+// 		this.songs.push(name)
+// 	},
+// 	showPlaylist() {
+// 		for (const song of this.songs) {
+// 			console.log(song)
+// 		}
+// 	},
+// 	playCurrentSong() {
+// 		const currentSong = this.songs[this.currenrSongIndex]
+// 		console.log(`odtwarzam: ${currentSong}`)
+// 		this.currenrSongIndex++
+// 		if (this.currenrSongIndex >= this.songs.length) {
+// 			this.currenrSongIndex = 0
+// 		}
+// 		setTimeout(() => {
+// 			this.playCurrentSong()
+// 		}, 10000)
+// 	},
+// }
+
+// playlist.addSong('NF - HOPE')
+// playlist.addSong('Perfect - Autobiografia')
+// playlist.addSong('Imagine Dragons - Wrecked')
+
+// playlist.showPlaylist()
+// playlist.playCurrentSong()
+
+const playList = {
+	currentIndex: 0,
 	songs: [],
 	addSong(name) {
 		this.songs.push(name)
 	},
-	showPlaylist() {
-		for (const song of this.songs) {
+	showList() {
+		this.songs.forEach(song => {
 			console.log(song)
-		}
+		})
 	},
-	playCurrentSong() {
-		const currentSong = this.songs[this.currenrSongIndex]
-		console.log(`odtwarzam: ${currentSong}`)
-		this.currenrSongIndex++
-		if (this.currenrSongIndex >= this.songs.length) {
-			this.currenrSongIndex = 0
+	playSongs() {
+		const currentSong = this.songs[this.currentIndex]
+		console.log(`Teraz leci: ${currentSong}`)
+		this.currentIndex++
+		if (this.currentIndex >= this.songs.length) {
+			this.currentIndex = 0
 		}
 		setTimeout(() => {
-			this.playCurrentSong()
+			this.playSongs()
 		}, 5000)
 	},
 }
 
-playlist.addSong('NF - HOPE')
-playlist.addSong('Perfect - Autobiografia')
-playlist.addSong('Imagine Dragons - Wrecked')
-
-playlist.showPlaylist()
-playlist.playCurrentSong()
+playList.addSong("Beyoncé - TEXAS HOLD'EM")
+playList.addSong('Sanah - Hip Hip Hurra')
+playList.addSong('Rihanna - Stay')
+playList.addSong("Fergie - Big Girls Don't Cry")
+playList.showList()
+playList.playSongs()
