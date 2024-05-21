50bb44682e733b76259abbb90332ce8dad09f54e
Monika <codingbymon@gmail.com>
2024-05-21 11:42:34 +0200

    finish excercise

--- script.js
+++ script.js
@@ -1,45 +1,27 @@
 'use strict'
 
-// const person = {}
+const car1 = {
+	make: 'Toyota',
+	model: 'Corolla',
 
-// const privateFields = {}
-
-// const privateFieldKey = Symbol()
-
-// person.setPrivateField = function (value) {
-// 	const privateFieldValue = value
-// 	privateFields[privateFieldKey] = privateFieldValue
-// }
-
-// person.getPrivateField = function () {
-// 	if (privateFieldKey in privateFields) {
-// 		return privateFields[privateFieldKey]
-// 	}
-// 	return null
-// }
-
-// person.setPrivateField(42)
-
-// const privateValue = person.getPrivateField()
-// console.log(privateValue)
-
-const cat = {
-	setName(name) {
-		privData[catName] = name
-		// const privNameofCat = name
+	startEngine() {
+		console.log('Silnik został uruchomiony')
 	},
-	getName() {
-		if (catName in privData) {
-			console.log(privData[catName]); 
+	toggleEngine() {
+		if (engineStatus === 'off') {
+			engineStatus = 'on'
+			console.log(engineStatus);
 		} else {
-			console.log(null);
+			engineStatus = 'off'
+			console.log(engineStatus);
 		}
 	},
 }
 
-const privData = {}
-
-const catName = Symbol()
-cat.setName('Lucjanek')
-cat.getName()
-// console.log(privData[catName])
+let engineStatus = Symbol('engineStatus')
+car1[engineStatus] = 'off'
+car1.startEngine()
+car1.toggleEngine()
+car1.toggleEngine()
+car1.toggleEngine()
+car1.toggleEngine()
