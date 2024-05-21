ea5dee1a91a88c1bf4955f57011249a78674c0ba
Monika <codingbymon@gmail.com>
2024-05-21 10:50:04 +0200

    symbol

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 'use strict'
 
-const person = {
-	setPrivateFields(value) {
-		const privateFieldValue = value
-		privateFields.privateFieldKey = privateFieldValue
-	},
-	getPrivateField(){
-		return this.privateFieldValue
-	}
-}
+const person = {}
 
 const privateFields = {}
 
 const privateFieldKey = Symbol()
+
+person.setPrivateField = function (value) {
+	const privateFieldValue = value
+	privateFields[privateFieldKey] = privateFieldValue
+}
+
+person.getPrivateField = function () {
+	if (privateFieldKey in privateFields) {
+		return privateFields[privateFieldKey]
+	}
+	return null
+}
+
+person.setPrivateField(42)
+
+const privateValue = person.getPrivateField()
+console.log(privateValue)
