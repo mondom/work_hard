38969790543ed84066651a25b36a9885ef4c0813
Monika <codingbymon@gmail.com>
2024-05-21 11:12:41 +0200

    my try

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 'use strict'
 
-const person = {}
+// const person = {}
 
-const privateFields = {}
+// const privateFields = {}
 
-const privateFieldKey = Symbol()
+// const privateFieldKey = Symbol()
 
-person.setPrivateField = function (value) {
-	const privateFieldValue = value
-	privateFields[privateFieldKey] = privateFieldValue
+// person.setPrivateField = function (value) {
+// 	const privateFieldValue = value
+// 	privateFields[privateFieldKey] = privateFieldValue
+// }
+
+// person.getPrivateField = function () {
+// 	if (privateFieldKey in privateFields) {
+// 		return privateFields[privateFieldKey]
+// 	}
+// 	return null
+// }
+
+// person.setPrivateField(42)
+
+// const privateValue = person.getPrivateField()
+// console.log(privateValue)
+
+const cat = {
+	setName(name) {
+		privData[catName] = name
+		// const privNameofCat = name
+	},
+	getName() {
+		if (catName in privData) {
+			console.log(privData[catName]); 
+		} else {
+			console.log(null);
+		}
+	},
 }
 
-person.getPrivateField = function () {
-	if (privateFieldKey in privateFields) {
-		return privateFields[privateFieldKey]
-	}
-	return null
-}
+const privData = {}
 
-person.setPrivateField(42)
-
-const privateValue = person.getPrivateField()
-console.log(privateValue)
+const catName = Symbol()
+cat.setName('Lucjanek')
+cat.getName()
+// console.log(privData[catName])
