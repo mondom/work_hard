b9c9a8125b6ec7ebb1625f1619b72eec6cd55da9
Monika <codingbymon@gmail.com>
2024-05-21 15:50:31 +0200

    valueOf()

--- script.js
+++ script.js
@@ -9,11 +9,32 @@ const portfolio = {
 	cash: 1000,
 	toString() {
 		let fullString = `Portfolio: \n`
-		for(const stock of this.stocks){
+		for (const stock of this.stocks) {
 			const { name, shares, price } = stock
 			fullString += `${name}: ${shares} shares at ${price}$ per share \n`
 		}
-		console.log(fullString);
+		fullString += `Cash: ${this.cash}$`
+		console.log(fullString)
+	},
+	valueOf(currency = 'USD') {
+		let totalValue = this.cash
+		for (const stock of this.stocks) {
+			const { shares, price } = stock
+			totalValue += shares * price
+		}
+		switch (currency) {
+			case 'USD':
+				return totalValue
+			case 'EUR':
+				return totalValue * 0.85
+			case 'GBP':
+				return totalValue * 0.72
+			default:
+				return totalValue
+		}
 	},
 }
 portfolio.toString()
+console.log(`Total value in USD: ${portfolio.valueOf("USD")}`);
+console.log(`Total value in EUR: ${portfolio.valueOf("EUR")}`);
+console.log(`Total value in GBP: ${portfolio.valueOf("GBP")}`);
