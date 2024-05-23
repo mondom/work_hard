b2b2a7e8e038b50eb9cf12fdff9a51b2a2f16d74
Monika <codingbymon@gmail.com>
2024-05-23 12:08:42 +0200

    test

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 'use strict'
 
+const randomArray = []
+
 const generateRandomArray = (length, range) => {
-	const randomArray = []
 	for (let i = 0; i < length; i++) {
 		const number = Math.floor(Math.random() * range)
 		randomArray.push(number)
 	}
-	console.log(randomArray);
+	return randomArray
 }
 
-generateRandomArray(10, 100)
+const findMinMax = arr => {
+	let max = -Infinity
+	let min = Infinity
+
+	let index = 0
+
+	while (index < arr.length) {
+		if (arr[index] < min) {
+			min = arr[index]
+		} else if (arr[index] > max) {
+			max = arr[index]
+		}
+		index++
+	}
+	return [min, max]
+}
+const generatedArray = generateRandomArray(10, 100)
+const [min, max] = findMinMax(randomArray)
+
+console.log(generatedArray);
+console.log(`Najmniejsza liczba: ${min} \nNajwiększa liczba: ${max}`)
