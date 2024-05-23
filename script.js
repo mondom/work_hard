faed9139d33d7828224d3a9fa85f033e4d0c9a70
Monika <codingbymon@gmail.com>
2024-05-23 10:10:33 +0200

    finish loop for

--- script.js
+++ script.js
@@ -1,40 +1,12 @@
 'use strict'
 
-const portfolio = {
-	stocks: [
-		{ name: 'Company A', shares: 100, price: 10 },
-		{ name: 'Company B', shares: 50, price: 20 },
-		{ name: 'Company C', shares: 200, price: 5 },
-	],
-	cash: 1000,
-	toString() {
-		let fullString = `Portfolio: \n`
-		for (const stock of this.stocks) {
-			const { name, shares, price } = stock
-			fullString += `${name}: ${shares} shares at ${price}$ per share \n`
-		}
-		fullString += `Cash: ${this.cash}$`
-		console.log(fullString)
-	},
-	valueOf(currency = 'USD') {
-		let totalValue = this.cash
-		for (const stock of this.stocks) {
-			const { shares, price } = stock
-			totalValue += shares * price
-		}
-		switch (currency) {
-			case 'USD':
-				return totalValue
-			case 'EUR':
-				return totalValue * 0.85
-			case 'GBP':
-				return totalValue * 0.72
-			default:
-				return totalValue
-		}
-	},
+const generateRandomArray = (length, range) => {
+	const randomArray = []
+	for (let i = 0; i < length; i++) {
+		const number = Math.floor(Math.random() * range)
+		randomArray.push(number)
+	}
+	console.log(randomArray);
 }
-portfolio.toString()
-console.log(`Total value in USD: ${portfolio.valueOf("USD")}`);
-console.log(`Total value in EUR: ${portfolio.valueOf("EUR")}`);
-console.log(`Total value in GBP: ${portfolio.valueOf("GBP")}`);
+
+generateRandomArray(10, 100)
