174aed154ddde5a9ebd8ceed36e9b579a7eabb7e
Monika <codingbymon@gmail.com>
2024-05-24 12:54:03 +0200

    finish task

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 'use strict'
 
-let sumaWydatków = 0
+const numbers = [5, 10, 15, 20, 15, 30, 35];
 
-const wydatki = [
-	{ nazwa: 'Jedzenie', wartosc: 50 },
-	{ nazwa: 'Rachunki', wartosc: 100 },
-	{ nazwa: 'Transport', wartosc: 30 },
-	{ nazwa: 'Zakupy', wartosc: 70 },
-]
-
-wydatki.forEach(kwota => {
-	sumaWydatków += kwota.wartosc
-})
-
-console.log(sumaWydatków);
+console.log('Index pierwszego wystąpienia: ' + numbers.indexOf(15));
+console.log('Index ostatniego wystąpienia: '+numbers.lastIndexOf(15));
