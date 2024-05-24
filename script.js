66c0aeb9cdc6f0673d378b769ba04394407cd95a
Monika <codingbymon@gmail.com>
2024-05-24 11:52:53 +0200

    task

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
 'use strict'
 
-const matrix = [
-	[1, 2, 3],
-	[4, 5, 6],
-	[7, 8, 9],
-]
+const myArray = ['jablko', 'banan', 'cytryna', 'pomarancza']
 
-let students = new Array()
-
-students[0] = 'John'
-students[1] = 'Emma'
-students[2] = 'Michael'
-
-console.log(students)
-
-let grades = new Array()
-
-grades[0] = 4
-grades[1] = 3
-grades[2] = 5
-
-console.log(grades)
-
-const assignStudents = (students, grades) => {
-	for (let i = 0; i < students.length; i++) {
-		console.log(`Students:\n${students[i]} - ${grades[i]}`);
-	}
+const accessArrayElements = index => {
+	return myArray.at(index)
 }
 
-assignStudents(students, grades)
+console.log(accessArrayElements(0))
+
+console.log(accessArrayElements(2))
+console.log(accessArrayElements(3))
