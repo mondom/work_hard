eafb95543200c5008bb7c1783c053a8453766369
Monika <codingbymon@gmail.com>
2024-05-26 17:10:04 +0200

    keep going

--- script.js
+++ script.js
@@ -1,57 +1,15 @@
 'use strict'
 
-const library = [
-	{
-		tytul: "Harry Potter and the Philosopher's Stone",
-		autor: 'J.K. Rowling',
-		rok: 1997,
-	},
-	{ tytul: 'To Kill a Mockingbird', autor: 'Harper Lee', rok: 1960 },
-	{
-		tytul: 'Fantastic Beasts and Where to Find Them',
-		autor: 'J.K. Rowling',
-		rok: 2001,
-	},
-	{ tytul: 'The Lord of the Rings', autor: 'J.R.R. Tolkien', rok: 1994 },
-	{ tytul: 'Go Set a Watchman', autor: 'Harper Lee', rok: 2015 },
+const produkty = [
+	{ nazwa: 'Telefon Samsung', cena: 1500, dostepny: true },
+	{ nazwa: 'Laptop Lenovo', cena: 2500, dostepny: true },
+	{ nazwa: 'Smartwatch Apple', cena: 800, dostepny: false },
+	{ nazwa: 'Telewizor LG', cena: 3000, dostepny: true },
+	{ nazwa: 'Sluchawki Sony', cena: 200, dostepny: true },
 ]
 
-const titleSought = 'Fantastic Beasts and Where to Find Them'
-
-const findBook = () => {
-	const foundBook = library.find(book => book.tytul === titleSought)
-
-	if (foundBook) {
-		console.log(`Krok 1:\nTytuł: ${foundBook.tytul}\nAutor: ${foundBook.autor}\nRok wydania: ${foundBook.rok}`)
-	} else {
-		console.log('Krok 1:')
-		console.log('Ksiazka o podanym tytule nie zostala znaleziona.')
-	}
-}
-const author = 'Harper Lee'
-const findIndex = () => {
-	const index = library.findIndex(book => book.autor === author)
-
-	if (index !== -1) {
-		console.log('Krok 2:')
-		console.log('Indeks ksiazki:', index)
-	} else {
-		console.log('Krok 2:')
-		console.log('Ksiazka o podanym autorze nie zostala znaleziona.')
-	}
-}
-const findLastIndex = () => {
-	const lastIndex = library.findLastIndex(book => book.autor === author)
-
-	if (lastIndex !== -1) {
-		console.log('Krok 3:')
-		console.log('Indeks ksiazki:', lastIndex)
-	} else {
-		console.log('Krok 3:')
-		console.log('Ksiazka o podanym autorze nie zostala znaleziona.')
-	}
+const findAvailableProducts = () => {
+	const availableProducts = produkty.filter(product => product.dostepny === true)
 }
 
-findBook()
-findIndex()
-findLastIndex()
+findAvailableProducts()
