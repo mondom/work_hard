a77520c82afd49e2012f7e5b7be79288d36804f7
Monika <codingbymon@gmail.com>
2024-05-29 14:37:09 +0200

    analise

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ map.set(ob2, "psy");
 map.set("[object Object]", "świnki");
 
 console.log(map);
+
+
+const map2 = {}
+
+const ob12 = { name : "test1" }
+const ob22 = { name : "test2" }
+
+map2[ob1] = "koty";
+map2[ob2] = "psy"; //ob2 skonwertowany na "[object Object]"
+map2["[object Object]"] = "świnki";
+
+console.log(map2); //{"[object Object]": "świnki"}
+
+// do analizy na świeżo...
