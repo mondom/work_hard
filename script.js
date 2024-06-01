8ef84dd9f0045d4d57d1a7f613f2fc2461313d62
Monika <codingbymon@gmail.com>
2024-06-01 11:29:26 +0200

    Map() excercise

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 'use strict'
 
-let magazyn = new Map()
+let orders = new Map()
 
-magazyn.set('Telewizor', 5)
-magazyn.set('Laptop', 10)
-magazyn.set('Smartfon', 20)
-magazyn.set('Konsola', 15)
+const order1 = {
+	id: 'order001',
+	customerName: 'John Doe',
+	items: ['Shirt', 'Pants', 'Shoes'],
+}
 
-console.log('Zawartość magazynu:')
+const order2 = {
+	id: 'order002',
+	customerName: 'Jane Smith',
+	items: ['Dress', 'Hat'],
+}
 
-magazyn.forEach((ilosc, produkt) => {
-	console.log(produkt + ': ' + ilosc)
-})
+orders.set(order1.id, order1)
+orders.set(order2.id, order2)
 
-magazyn.set('Telewizor', 3)
-
-console.log('Zawartość magazynu:')
-
-magazyn.forEach((ilosc, produkt) => {
-	console.log(produkt + ': ' + ilosc)
-})
-
-magazyn.delete('Smartfon')
-
-console.log('Zawartość magazynu:');
-magazyn.forEach((ilosc,produkt)=>{
-	console.log(produkt + ': ' + ilosc);
-})
+console.log(`Liczba zamówień w systemie: ${orders.size}`)
