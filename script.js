1e77773c0dfe59314baba3b9f6208f567f28624d
Monika <codingbymon@gmail.com>
2024-06-01 11:56:47 +0200

    next level of task

--- script.js
+++ script.js
@@ -18,3 +18,11 @@ orders.set(order1.id, order1)
 orders.set(order2.id, order2)
 
 console.log(`Liczba zamówień w systemie: ${orders.size}`)
+
+console.log('Zamówienia w systemie:')
+
+for (const [orderId, order] of orders.entries()) {
+	console.log(`[ID zamówienia: ${orderId}]`)
+	console.log(`Klient: ${order.customerName}`)
+	console.log(`Przedmioty: ${order.items}`)
+}
