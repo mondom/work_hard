70c732774d88aa779aa2a6af85b8a6f475979ab6
Monika <codingbymon@gmail.com>
2024-06-02 07:31:39 +0200

    WeakSet()

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
 'use strict'
 
-const employee1 = { id: 1, firstName: 'John', lastName: 'Doe' }
-const employee2 = { id: 2, firstName: 'Jane', lastName: 'Smith' }
+const usersSet = new WeakSet()
 
-const employeeMap = new WeakMap()
+const user1 = { id: 1 }
+const user2 = { id: 2 }
+const user3 = { id: 3 }
 
-employeeMap.set(employee1, { position: 'Manager', salary: 5000 })
-employeeMap.set(employee2, { position: 'Developer', salary: 4000 })
+const trackUserActivity = (user, action) => {
+	if (!usersSet.has(user)) {
+		usersSet.add(user)
+	}
 
-console.log(`Sprawdzanie danych pracownika:`)
-if (employeeMap.has(employee1)) {
-	console.log(`Mapa posiada dane pracownika nr 1`)
-} else {
-	console.log(`Mapa nie posiada danych pracownika nr 1`)
+	user.activityCount = (user.activityCount || 0) + 1
+
+	console.log(`Użytkownik o id ${user.id} wykonał ${user.activityCount} akcje: ${action}`);
 }
 
-const employee1Data = employeeMap.get(employee1)
-console.log(`Informacje o pracowniku nr 1:`);
-console.log('ID: ' + employee1.id);
-console.log('Imię: ' + employee1.firstName);
-console.log('Nazwisko: ' + employee1.lastName);
-console.log('Stanowisko: ' + employee1Data.position);
-console.log('Wynagrodzenie: ' + employee1Data.salary);
-
-employeeMap.delete(employee2)
-
-console.log('Sprawdzanie danych dla usuniętego pracownika');
-if(employeeMap.has(employee2)){
-	console.log('to źle');
-}else{
-	console.log('Brak danych pracownika nr 2');
-}
+trackUserActivity(user1, 'klikniecie na przycisk')
+trackUserActivity(user2, 'przesuniecie suwaka')
+trackUserActivity(user1, 'wyslanie formularza')
+trackUserActivity(user3, 'klikniecie na link')
