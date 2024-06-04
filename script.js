69e7b7c7e9c5289394b84711d89a364c3c2506d6
Monika <codingbymon@gmail.com>
2024-06-04 15:18:06 +0200

    end

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ function countDayOfWeek(){
 
 	for(let i = 0;i<7;i++){
 		let dateCopy = new Date(currentDate.getTime())
+
+		dateCopy.setDate(currentDate.getDate() + i);
+
+		let dayOfWeek = dateCopy.toLocaleDateString("pl-PL", {
+			weekday: "long",
+		});
+		daysOfWeek.push(dayOfWeek);
 	}
+	console.log(daysOfWeek);
 }
 countDayOfWeek()
 
