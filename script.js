6733b834dcf313fa1143727921f7e433759f9420
Monika <codingbymon@gmail.com>
2024-06-04 14:41:52 +0200

    repeat task

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 'use strict'
 
-function countDaysOfWeek() {
-	let currentDate = new Date();
-	let daysOfWeek = [];
+function countDayOfWeek(){
+	let currentDate = new Date()
+	let daysOfWeek = []
 
-	for (let i = 0; i < 7; i++) {
-		let dateCopy = new Date(currentDate.getTime());
-		
-		dateCopy.setDate(currentDate.getDate() + i);
-		let dayOfWeek = dateCopy.toLocaleDateString("pl-PL", {
-			weekday: "long",
-		});
-		daysOfWeek.push(dayOfWeek);
+	for(let i = 0;i<7;i++){
+		let dateCopy = new Date(currentDate.getTime())
 	}
-
-	daysOfWeek.forEach((day) => {
-		console.log(day);
-	});
 }
+countDayOfWeek()
 
-countDaysOfWeek();
