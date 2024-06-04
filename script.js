dd88c4827edc7b34b0dde5a2ce4472b91f02352a
Monika <codingbymon@gmail.com>
2024-06-04 16:09:39 +0200

    my try

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 'use strict'
 
-function displayCurrentDateTime() {
+function calculateFutureDate() {
+
 	let currentDate = new Date()
-	let year = currentDate.getFullYear()
-	let month = currentDate.getMonth()
-	let day = currentDate.getDate()
-	let hours = currentDate.getHours()
-	let minutes = currentDate.getMinutes()
-	let seconds = currentDate.getSeconds()
-	let miliseconds = currentDate.getMilliseconds()
 
-	console.log(`Current date: ${day}/${month + 1}/${year}`)
-
-	console.log(`Current time: ${hours}:${minutes}:${seconds}:${miliseconds}`)
+	currentDate.setFullYear(currentDate.getFullYear())
+	console.log(currentDate.getFullYear() + 3);
+	currentDate.setMonth(currentDate.getMonth())
+	currentDate.setDate(15)
+	currentDate.setHours(18)
+	currentDate.setMinutes(30)
+	currentDate.setSeconds(45)
+	currentDate.setMilliseconds(500)
+	console.log(`Future Date: ${currentDate.toLocaleString()}`)
 }
 
-displayCurrentDateTime()
+calculateFutureDate()
