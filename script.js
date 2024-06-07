db96bc58144eaace09f1bbd5a2f71ecb84d77d7d
Monika <codingbymon@gmail.com>
2024-06-07 15:42:28 +0200

    finish

--- script.js
+++ script.js
@@ -3,13 +3,20 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const calculatSum = arr => {
-	return arr.reduce((sum, current) => {
-		if (current > 5) {
-			return (sum += current)
-		} else {
-			return sum
-		}
-	},0)
+	if (arr.length === 0) {
+		return 0
+	}
+	let sum = 0
+	const firstElement = arr[0]
+
+	let newArr = arr.slice(1)
+	if (firstElement > 5) {
+		sum += firstElement
+	}
+
+	const recusiveSum = calculatSum(newArr)
+	sum += recusiveSum
+	return sum
 }
 
 const result = calculatSum(arr)
