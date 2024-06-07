bbe74c8c96c88a7e3fbefb8c88f93f96e5391f2e
Monika <codingbymon@gmail.com>
2024-06-07 14:43:59 +0200

    tests

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 'use strict'
 
-const countDown = num => {
-    if (num <= 0) {
-        console.log('Liczenie zakończone!');
-        return;
-    } else {
-        console.log(num);
-        countDown(num - 1);
-    }
-}
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-countDown(5);
+// const calculateSum = arr => {
+// 	if (arr.length === 0) {
+// 		return 0
+// 	}
+//     console.log(arr);
+
+// 	let firstElement = arr[0]
+// 	let sum = 0
+
+// 	if (firstElement > 5) {
+// 		sum += firstElement
+// 	}
+    
+
+// 	let remainingArray = arr.slice(1)
+//     const recursiveSum = calculateSum(remainingArray)
+// 	sum += recursiveSum
+//     return sum
+// }
+
+// const result = calculateSum(arr)
+// console.log(result);
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const calculateSum = arr => {
+    return arr.reduce((sum, current) => {
+        if (current > 5) {
+            console.log('suma: '+sum);
+            console.log('obecna liczba: '+current);
+            return sum + current;
+
+        }
+        return sum;
+    }, 0);
+};
+
+const result = calculateSum(arr);
+console.log(result); // Wyświetla sumę elementów większych niż 5
