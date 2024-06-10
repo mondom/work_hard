f2c10def46c3fb699220b462dbcb90cabd6940e8
Monika <codingbymon@gmail.com>
2024-06-10 15:23:43 +0200

    replays

--- script.js
+++ script.js
@@ -1,47 +1,12 @@
 'use strict'
 
-// const calculateFactorial = num => {
-// 	if (num === 0 || num === 1) {
-// 		return 1
-// 	}
+function countDown(num) {
+	if (num <= 0) {
+		return console.log('Koniec liczenia')
+	}
 
-// 	let factorial = calculateFactorial(num - 1)
-// 	console.log(num);
-// 	factorial *= num
-// 	return factorial
-// }
-
-// let num = 5
-
-// const result = calculateFactorial(num)
-
-// console.log('Silnia liczby', num, 'to:', result)
-
-//setTimeout i click trafią do kolejki zadań
-setTimeout(() => {
-  console.log("Jestem tekstem z setTimeout A");
-}, 0);
-
-setTimeout(() => {
-  console.log("Jestem tekstem z setTimeout B");
-}, 0);
-
-//rejestruję klikanie na dokumencie
-document.addEventListener("click", () => {
-  console.log("klik");
-});
-
-//pętla trwająca 3 sekundy
-//kod synchroniczny - jako pierwszy trafi do Call Stack
-let start = new Date().getTime();
-let end = start;
-while(end < start + 3000) {
-  end = new Date().getTime();
+  console.log(num);
+	countDown(num - 1)
 }
 
-console.log("Jestem tekstem ze zwykłego console.log");
-
-  
-  
-
-
+countDown(5)
