16d2f857024fbff8df61ba202842a9f3885621a2
Monika <codingbymon@gmail.com>
2024-06-11 16:02:43 +0200

    finish task

--- script.js
+++ script.js
@@ -1,61 +1,12 @@
 'use strict'
 
-// function checkSyntax(code) {
-// 	const stack = []
-
-// 	for (let i = 0; i < code.length; i++) {
-// 		const char = code[i]
-
-// 		if (char === '(' || char === '{' || char === '[') {
-// 			stack.push(char)
-// 		} else if (char === ')' || char === '}' || char === ']') {
-// 			if (stack.length === 0) {
-//         console.log('Brak otwierającego nawiasu');
-// 				return false
-// 			}
-
-// 			const top = stack.pop()
-
-// 			if ((char === ')' && top !== '(') || (char === '}' && top !== '{') || (char === ']' && top !== '[')) {
-// 				return false // Niepoprawne sparowanie nawiasów
-// 			}
-// 		}
-// 	}
-
-// 	return stack.length === 0 // Brak zamykającego nawiasu
-// }
-
-// const code = "(function() { console.log('Hello, world!'); })()"
-// const isSyntaxValid = checkSyntax(code)
-
-// console.log('Czy skladnia kodu jest poprawna?', isSyntaxValid)
-
-function checkSyntax(code) {
-	const stack = []
-
-	for (const sign of code) {
-		if (sign === '(' || sign === '{' || sign === '[') {
-			stack.push(sign)
-		} else if (sign === ')' || sign === '}' || sign === ']') {
-			if (stack.length === 0) {
-				return false
-			}
-			const openBracket = stack.pop()
-
-			if (
-				(sign === ')' && openBracket !== '(') ||
-				(sign === '}' && openBracket !== '{') ||
-				(sign === ']' && openBracket !== '[')
-			) {
-				return false
-			}
-		}
+function calculateAverage(...numbers) {
+	let sum = 0
+	for (const num of numbers) {
+		sum += num
 	}
-
-	return stack.length === 0
+	return (sum / numbers.length).toFixed(2)
 }
 
-const code = '(function() { console.log("Hello, world!"); })()'
-const isSyntaxValid = checkSyntax(code)
-
-console.log('Czy składnia kodu jest poprawna?', isSyntaxValid)
+const result = calculateAverage(1, 5, 6, 7, 5, 4, 4, 5, 6, 4, 7, 10)
+console.log(result)
