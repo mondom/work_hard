a2a32121068e2aec94308a0b6258f55b51940000
Monika <codingbymon@gmail.com>
2024-06-12 16:20:20 +0200

    finish task

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 'use strict'
 
-function gotujJajka() {
-	let czasGotowania = 0
+const trasa = [
+	{ x: 0, y: 0 },
+	{ x: 100, y: 50 },
+	{ x: 200, y: 100 },
+	{ x: 300, y: 150 },
+	{ x: 400, y: 200 },
+]
 
-	function sprawdxStanJajek() {
-		czasGotowania++
-
-		if (czasGotowania === 5) {
-			console.log('Jajka są na wpół ugotowane')
-		} else if (czasGotowania === 8) {
-			console.log('Jajka są prawie gotowe!')
-		} else if (czasGotowania === 10) {
-			console.log('Jajka są gotowe!')
-			return
-		}
-		setTimeout(sprawdxStanJajek, 1000)
-	}
-	sprawdxStanJajek()
+const samochod = {
+	pozycja: 0,
+	predkosc: 10,
 }
 
-setTimeout(gotujJajka, 3000)
+const przesunSamochod = () => {
+	let punkt = trasa[samochod.pozycja]
+
+	if (samochod.pozycja === trasa.length) {
+		console.log('Koniec trasy')
+    clearInterval(run)
+    return
+	}
+
+	console.log(`Samochod jest na pozycji x: ${punkt.x}, y: ${punkt.y}`)
+
+	samochod.pozycja++
+}
+
+const run = setInterval(przesunSamochod, 1000)
+
