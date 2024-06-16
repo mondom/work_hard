e5e246c14f38b787a3f789c3985a8f90a3e99295
Monika <codingbymon@gmail.com>
2024-06-16 10:19:03 +0200

    bind() tests

--- script.js
+++ script.js
@@ -11,20 +11,21 @@ function calculateTotal() {
 		return acc + curr * this.quantities[index]
 	}, 0)
 
-	console.log(result)
+	return result
 }
 
-calculateTotal.bind(order)()
+const totalValue = calculateTotal.bind(order)()
 
-// function displayOrderDetails() {
-// 	console.log('Szczegoly zamowienia:')
-// 	this.products.forEach((product, index) => {
-// 		console.log(`${product} - ilosc: ${this.quantities[index]}, cena jednostkowa: ${this.unitPrices[index]}`)
-// 	})
-// 	const total = calculateTotal.call(this)
-// 	console.log('laczna wartosc zamowienia:', total)
-// }
+function displayOrderDetails() {
+	console.log('Szczegóły zamówienia:');
+	this.products.forEach((product, index) => {
+		console.log(product + ' - ilość: ' +this.quantities[index] + ' cena jednostkowa: ' + this.unitPrices[index] + 'zł');
+	});
+	
+	console.log('laczna wartosc zamowienia: '+ totalValue + 'zł'
+	)
+}
 
-// const displayOrderDetailsForOrder = displayOrderDetails.bind(order)
+displayOrderDetails.bind(order)()
 
-// displayOrderDetailsForOrder()
+
