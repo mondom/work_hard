790a95508800faa8185601cb737c83db9400361f
Monika <codingbymon@gmail.com>
2024-06-18 09:23:04 +0200

    continue

--- script.js
+++ script.js
@@ -8,15 +8,21 @@ const transactions = [
 function calculateTotalRevenue(transactions) {
 	const total = transactions.reduce((acc, obj) => {
 		const count = obj.price * obj.quantity
-		acc+=count
+		acc += count
 		return acc
 	}, 0)
-	return total;
+	return total
 }
 
-const result = calculateTotalRevenue.apply(null, [transactions])
-console.log(result);
+const resultTotal = calculateTotalRevenue.apply(null, [transactions])
+console.log(resultTotal)
 
-function findBestSellingProduct(transactions){
-	
+function findBestSellingProduct(transactions) {
+	const bestSelling = transactions.reduce((max, curr) => {
+		return max.quantity > curr.quantity ? max : curr
+	}, 0)
+	return bestSelling.name
 }
+
+const resultBestSelling = findBestSellingProduct.apply(null, [transactions])
+console.log(resultBestSelling)
