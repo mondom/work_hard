d051b58a2dea08f97f156a9871c9511490b352b5
Monika <codingbymon@gmail.com>
2024-06-20 11:17:48 +0200

    getter & setter

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
 'use strict'
 
-const car = {}
-console.log("Poczatkowy obiekt 'car':", car)
+const person = {
+    name: 'John',
+    age: 30,
+    get info() {
+        return `${this.name} is ${this.age} years old`;
+    },
+    set updateName(newName) {
+        this.name = newName;
+    }
+};
 
-Object.defineProperty(car, 'brand', {
-	value: 'Toyota',
-	writable: false,
-	enumerable: true,
-	configurable: false,
-})
-
-Object.defineProperty(car, 'model', {
-	value: 'Camry',
-	writable: false,
-	enumerable: true,
-	configurable: false,
-})
-console.log("Zmodyfikowany obiekt 'car':", car)
-
-car.brand = "Honda";
-console.log("Obiekt 'car' po probie zmiany wlasciwosci 'brand':", car);
-
-delete car.model;
-console.log("Obiekt 'car' po probie usuniecia wlasciwosci 'model':", car);
-
+console.log(person.info); // Dostęp do właściwości jak do zwykłej wartości
+person.updateName = 'Jane'; // Aktualizacja właściwości jak zwykłej wartości
+console.log(person.name);
 
 
 
