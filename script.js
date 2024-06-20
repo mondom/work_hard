36df272bdda74fa45ed9539b1029ad2a2c840b08
Monika <codingbymon@gmail.com>
2024-06-20 14:15:40 +0200

    Object.getOwnPropertyDescriptors

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 'use strict'
 
-const sentence = 'Ala ma kota'
+const person = {
+	name: 'John',
+	age: 40,
+	city: 'New York',
+}
 
-const result = sentence.split(' ').reverse().join(' ')
+const propertyDescriptors = Object.getOwnPropertyDescriptors(person)
+console.log(propertyDescriptors);
 
-console.log(result);
+
+
+console.log("Wartosc wlasciwosci 'name':", person.name);
+console.log("Wartosc wlasciwosci 'age':", person.age);
+console.log("Wartosc wlasciwosci 'city':", person.city)
+
+person.name = 'Mike'
+
+console.log("Wartosc wlasciwosci 'name':", person.name);
+console.log("Wartosc wlasciwosci 'age':", person.age);
+console.log("Wartosc wlasciwosci 'city':", person.city);
+
+delete person.age
+
+console.log("Wartosc wlasciwosci 'age':", person.age);
+
+console.log(propertyDescriptors);
