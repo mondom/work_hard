196793448f89bab5bd3f83633df1d946adb9872a
Monika <codingbymon@gmail.com>
2024-06-20 11:56:17 +0200

    getter & setter

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 'use strict'
 
-const person = {
-    name: 'John',
-    age: 30,
-    get info() {
-        return `${this.name} is ${this.age} years old`;
-    },
-    set updateName(newName) {
-        this.name = newName;
-    }
-};
+const user = {
+	name: 'Monika',
+	mail: 'mdominik@vp.pl',
+	password: 'kitty1234',
 
-console.log(person.info); // Dostęp do właściwości jak do zwykłej wartości
-person.updateName = 'Jane'; // Aktualizacja właściwości jak zwykłej wartości
-console.log(person.name);
+	get pass() {
+		console.log(this.password)
+	},
 
+	set newName(name) {
+		this.name = name
+	},
+}
+console.log(user.name);
 
+user.newName = 'Karolina'
 
+Object.defineProperty(user, "password",{
+	writable: false,
+})
 
+console.log(user.name);
+
+user.pass
+
+// const person = {
+// 	name: 'John',
+// 	age: 30,
+// 	get info() {
+// 		return `${this.name} is ${this.age} years old`
+// 	},
+// 	set updateName(newName) {
+// 		this.name = newName
+// 	},
+// }
+
+// console.log(person.info) // Dostęp do właściwości jak do zwykłej wartości
+// person.updateName = 'Jane' // Aktualizacja właściwości jak zwykłej wartości
+// console.log(person.name)
