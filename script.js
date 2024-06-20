ecf78b8316dc05ae04d585038eaea4e4d0334dbd
Monika <codingbymon@gmail.com>
2024-06-20 14:35:32 +0200

    getter

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 'use strict'
 
-const TemperatureTracker ={
+const TemperatureTracker = {
 	temperatures: [],
 
-	get average(){
-		this.temperatures.reduce((acc,curr)=>{
-			return acc+=curr / this.temperatures.length
-		})
+	get average() {
+		const sum = this.temperatures.reduce((acc, cur) => acc + cur, 0)
+		return sum / this.temperatures.length
 	},
 
-	addTemperature(temp){
+	addTemperature(temp) {
 		this.temperatures.push(temp)
-	}
+	},
 }
+
+TemperatureTracker.addTemperature(25)
+TemperatureTracker.addTemperature(30)
+TemperatureTracker.addTemperature(20)
+TemperatureTracker.addTemperature(35)
+
+console.log(TemperatureTracker.temperatures)
+
+console.log('Srednia temperatura:', TemperatureTracker.average)
+// console.log('Najwyzsza temperatura:', TemperatureTracker.max)
+// console.log('Najnizsza temperatura:', TemperatureTracker.min)
