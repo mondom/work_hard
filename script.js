0fe1e4eef780f2fd28fa47c997bfb1a8260a99a8
Monika <codingbymon@gmail.com>
2024-06-21 14:04:39 +0200

    prototype

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
 'use strict'
 
-// function Animal(name, type) {
-// 	;(this.name = name), (this.type = type)
-// }
+function Animal(name, type) {
+	this.name = name, 
+	this.type = type
+}
 
-// Animal.prototype.getDescription = function () {
-// 	return `To jest ${this.type} o imieniu ${this.name}.`
-// }
+Animal.prototype.getDescription = function () {
+	return `To jest ${this.type} o imieniu ${this.name}.`
+}
 
-// const animal1 = new Animal('Burek', 'Pies')
-// console.log(animal1.getDescription())
+const animal1 = new Animal('Burek', 'Pies')
+console.log(animal1.getDescription())
 
+function Dog(name) {
+	Animal.call(this, name, 'Maltańczyk')
+	
+}
+Dog.prototype = Object.create(Animal.prototype)
 
-const animal = {
-	type: 'Animal',
-	displayType: function() {
-	  console.log(this.type);
-	console.log(this);
-	}
-  };
-  
-  const dog = Object.create(animal, {
-	name: {
-	  value: 'Rex',
-	  writable: true,
-	  enumerable: true,
-	  configurable: true
-	},
-	bark: {
-	  value: function() {
-		console.log('Woof!');
-	  },
-	  writable: false,
-	  enumerable: false,
-	  configurable: false
-	}
-  });
-  
-  console.log(dog.name); // Output: Rex
-  dog.bark(); // Output: Woof!
-  dog.displayType(); // Output: Animal
+const dog1 = new Dog('Reksio');
+console.log(dog1.getDescription());
