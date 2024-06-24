1d03bc10aba957c28f845554aa5869dcd0554e7f
Monika <codingbymon@gmail.com>
2024-06-24 17:57:34 +0200

    class in js

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 'use strict'
 
-function Employee(name, age, position) {
-	this.name = name
-	this.age = age
-	this.position = position
+// function Person(name, age) {
+//     this.name = name
+//     this.age = age
+// }
+// // =================================
+
+// Person.prototype.sayHi = function () {
+//     console.log(`Hi, I'm 	${this.name}`)
+// }
+
+// Person.prototype.showAge = function () {
+//     console.log(`I'm ${this.age} 	years old`)
+// }
+
+class Person{
+	constructor(name, age){
+		this.name = name
+		this.age = age
+	}
+
+	sayHi(){
+		console.log(`Hi, I'm ${this.name}.`);
+	}
+
+	showAge(){
+		console.log(`I'm ${this.age} years old.`);
+	}
 }
 
-Employee.prototype.introduce = function () {
-	console.log(`Czesc, jestem ${this.name}, mam ${this.age} lat i pracuje jako ${this.position}.`)
-}
+const me = new Person('Monika', '35')
 
-const employee1 = new Employee('Jan Kowalski', 30, 'Programista')
-employee1.introduce()
-
-const employee2 = new Employee('Anna Nowak', 25, 'Analityk')
-employee2.introduce()
-
-function Manager(name, age, position, department) {
-	Employee.call(this, name, age, position, this.manage)
-	this.department = department
-}
-Manager.prototype = Object.create(Employee.prototype)
-Manager.prototype.constructor = Manager
-
-Manager.prototype.manage = function () {
-	console.log(`Jestem ${this.name}, pracuje jako ${this.position} i zarzadzam dzialem ${this.department}.`)
-}
-const manager1 = new Manager('Kamil Wisniewski', 35, 'Kierownik', 'IT')
-manager1.introduce()
-manager1.manage()
+me.sayHi()
+me.showAge()
