a243a0e6d02bbb0fc40520e5ee12c98742485eba
Monika <codingbymon@gmail.com>
2024-06-25 13:42:59 +0200

    class

--- script.js
+++ script.js
@@ -11,17 +11,17 @@ class listaZakupow {
 	}
 	usunElement(nazwa) {
 		if (this.zakupy.length !== 0) {
-			const itemToRemove = this.zakupy.indexOf(nazwa)
+			const index = this.zakupy.indexOf(nazwa)
 			console.log(`Usunięto ${nazwa} z listy zakupów.`)
-			this.zakupy.splice(itemToRemove, 1)
+			this.zakupy.splice(index, 1)
 		} else {
 			console.log('Lista zakupów jest pusta')
 		}
 	}
 	wyswietlListe() {
 		console.log('Lista Zakupów')
-		this.zakupy.forEach(item => {
-			console.log(`${this.zakupy.indexOf(item) + 1}. ${item}`)
+		this.zakupy.forEach((item, index) => {
+			console.log(index + '. ' + item)
 		})
 	}
 }
