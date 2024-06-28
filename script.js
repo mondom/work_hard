068b35f004da7cfd85060bd5844b2529f2f2cf15
Monika <codingbymon@gmail.com>
2024-06-28 11:20:49 +0200

    tests

--- script.js
+++ script.js
@@ -30,8 +30,21 @@ class Klient {
 		this.koszyk.splice(index, 1)
 		Klient.licznikProduktow--
 	}
-	obliczWartoscKoszyka(){
-		suma = 0
-for(const produkt of pro)
+	obliczWartoscKoszyka() {
+		const suma = 0
+		for (let produkt of this.koszyk) {
+			suma += produkt.pobierzCene()
+		}
+		return suma
 	}
 }
+
+const klient1 = new Klient()
+const klient2 = new Klient()
+
+const produkt1 = new Produkt("Telewizor", 1999.99);
+const produkt2 = new Produkt("Smartfon", 1299.99);
+
+klient1.dodajProdukt(produkt1)
+klient1.dodajProdukt(produkt2)
+console.log("Wartosc koszyka klienta 1:", klient1.obliczWartoscKoszyka());
