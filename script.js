087aaab2b5e745f7ffa7d7aba933646f6a7cfc3e
Monika <codingbymon@gmail.com>
2024-06-28 10:45:21 +0200

    tests

--- script.js
+++ script.js
@@ -6,29 +6,32 @@ class Produkt {
 		this.cena = cena
 	}
 
-	pobierzNazwę(){
+	pobierzNazwę() {
 		return this.nazwa
 	}
 
-	pobierzCenę(){
+	pobierzCenę() {
 		return this.cena
 	}
 }
 
-class Klient{
-	
-	constructor(){
+class Klient {
+	constructor() {
 		this.koszyk = []
 	}
 	static licznikProduktow = 0
 
-	dodajProdukt(produkt){
+	dodajProdukt(produkt) {
 		this.koszyk.push(produkt)
 		Klient.licznikProduktow++
 	}
 
-	usunProdukt(index){
-		this.koszyk.splice(index,1)
+	usunProdukt(index) {
+		this.koszyk.splice(index, 1)
 		Klient.licznikProduktow--
+	}
+	obliczWartoscKoszyka(){
+		suma = 0
+for(const produkt of pro)
 	}
 }
