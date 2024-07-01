534c9f05824b989c266ad0080b08179738fb29a1
Monika <codingbymon@gmail.com>
2024-07-01 11:57:23 +0200

    static method

--- script.js
+++ script.js
@@ -1,116 +1,19 @@
 'use strict'
 
-// class Produkt {
-// 	constructor(nazwa, cena) {
-// 		this.nazwa = nazwa
-// 		this.cena = cena
-// 	}
-
-// 	pobierzNazwe() {
-// 		return this.nazwa
-// 	}
-
-// 	pobierzCene() {
-// 		return this.cena
-// 	}
-// }
-
-// class Klient {
-// 	static licznikProduktow = 0
-
-// 	constructor() {
-// 		this.koszyk = []
-// 	}
-
-// 	dodajProdukt(produkt) {
-// 		this.koszyk.push(produkt)
-// 		Klient.licznikProduktow++
-// 	}
-
-// 	usunProdukt(index) {
-// 		this.koszyk.splice(index, 1)
-// 		Klient.licznikProduktow--
-// 	}
-// 	obliczWartoscKoszyka() {
-// 		let suma = 0
-// 		for (let produkt of this.koszyk) {
-// 			suma += produkt.pobierzCene()
-// 		}
-// 		return suma
-// 	}
-// }
-
-// const klient1 = new Klient()
-// const klient2 = new Klient()
-
-// const produkt1 = new Produkt('Telewizor', 1999.99)
-// const produkt2 = new Produkt('Smartfon', 1299.99)
-
-// klient1.dodajProdukt(produkt1)
-// klient2.dodajProdukt(produkt2)
-
-// console.log('Wartosc koszyka klienta 1:', klient1.obliczWartoscKoszyka())
-// console.log('Wartosc koszyka klienta 2:', klient2.obliczWartoscKoszyka())
-
-// console.log('Liczba produktow we wszystkich koszykach:', Klient.licznikProduktow)
-
-class Fruit {
-	constructor(name, weight) {
-		this.name = name
-		this.weight = weight
+class RachunekGieldowy{
+	constructor(imie,nazwisko){
+		this.imie=imie
+		this.nazwisko=nazwisko
 	}
 
-	returnName() {
-		return this.name
-	}
-
-	returnWeight() {
-		return this.weight
+	static obliczWartoscAkcji(cenaAkcji){
+		return cenaAkcji * 100;
 	}
 }
 
-const fruit1 = new Fruit('apple', 300)
-const fruit2 = new Fruit('banana', 250)
-const fruit3 = new Fruit('strawberry', 80)
-const fruit4 = new Fruit('pear', 280)
-const fruit5 = new Fruit('watermelon', 1000)
+const rachunek = new RachunekGieldowy('Jan','Kowalski')
+const cenaAkcji = 10.5
 
-class Basket {
-	static fruitCount = 0
-	constructor() {
-		this.fruitBasket = []
-	}
+const wartoscAkcji = RachunekGieldowy.obliczWartoscAkcji(cenaAkcji)
 
-	addFruit(fruit) {
-		this.fruitBasket.push(fruit)
-		Basket.fruitCount++
-	}
-
-	removeFruit(index) {
-		this.fruitBasket.splice(index, 1)
-		Basket.fruitCount--
-	}
-
-	countWeightOfBasket() {
-		let sum = 0
-		for (const fruit of this.fruitBasket) {
-			sum += fruit.returnWeight()
-		}
-		return (sum/1000).toFixed(2)
-	}
-}
-
-const basket1 = new Basket()
-
-basket1.addFruit(fruit1)
-basket1.addFruit(fruit2)
-basket1.addFruit(fruit3)
-basket1.addFruit(fruit4)
-basket1.addFruit(fruit5)
-
-basket1.removeFruit(1)
-
-console.log(Basket.fruitCount);
-
-const finalWeight = basket1.countWeightOfBasket()
-console.log(`Ostateczna waga koszyka to: ${finalWeight} kg.`);
+console.log(`Imię i nazwisko: ${rachunek.imie} ${rachunek.nazwisko}\nWartość akcji wynosi: ${wartoscAkcji}`);
