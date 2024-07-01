f5fdfd994c1acaeca0a13ff6abe410c931cc297a
Monika <codingbymon@gmail.com>
2024-07-01 12:44:58 +0200

    finish static

--- script.js
+++ script.js
@@ -13,4 +13,32 @@ class Stock {
 			currency: 'USD',
 		})
 	}
+
+	getValue() {
+		return this.price * this.quantity
+	}
 }
+
+class DividendStock extends Stock {
+	constructor(symbol, price, quantity, divident) {
+		super(symbol, price, quantity)
+		this.divident = divident
+	}
+
+	static calculateDividendYield(divident, price) {
+		return divident / price
+	}
+
+	getDividendValue() {
+		return this.divident * this.quantity
+	}
+}
+
+const myStock = new Stock("AAPL", 150.25, 100)
+
+console.log("Wartosc moich akcji:", myStock.getValue());
+
+const myDividendStock = new DividendStock("GOOGL", 1200.75, 50, 2.5)
+console.log("Wartosc moich akcji z dywidenda:", myDividendStock.getValue());
+
+console.log("Wartosc dywidendy:", myDividendStock.getDividendValue());
