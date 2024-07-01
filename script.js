732f3df4a3585492d5b78b43e9e40e597dcc4424
Monika <codingbymon@gmail.com>
2024-07-01 14:24:08 +0200

    protected property

--- script.js
+++ script.js
@@ -1,30 +1,16 @@
 'use strict'
 
-class Person {
-	constructor(name, age, address) {
-		this._name = name
-		this._age = age
-		this._address = address
+class BankAccount {
+	constructor(accountNumber, initialBalance) {
+		this._accountNumber = accountNumber
+		this._balance = initialBalance
 	}
 
-	introduce() {
-		console.log(`Cześć, nazywam się ${this._name}, mam ${this._age} lat i mieszkam w ${this._address}`)
-	}
+	_checkMinimumBalance() {
+		const minimumBalance = 100
 
-	changeAddress(newAddress) {
-		this._address = newAddress
+		if(this._balance < minimumBalance){
+			console.log('Saldo jest zbyt niskie!');
+		}
 	}
 }
-
-const person1 = new Person('Jan Kowalski', 30, 'ul. Kwiatowa 1')
-
-console.log(person1._name);
-
-person1.introduce()
-
-person1.changeAddress('ul. Sloneczna 10')
-
-person1.introduce()
-
-
-
