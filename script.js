594d0611e9ee5c75344ff13424c1fbcf2eb2efb3
Monika <codingbymon@gmail.com>
2024-07-02 10:05:35 +0200

    return in object

--- script.js
+++ script.js
@@ -9,8 +9,28 @@ class BankAccount {
 	_checkMinimumBalance() {
 		const minimumBalance = 100
 
-		if(this._balance < minimumBalance){
-			console.log('Saldo jest zbyt niskie!');
+		if (this._balance < minimumBalance) {
+			console.log('Saldo jest zbyt niskie!')
 		}
 	}
+
+	getBalance() {
+		return this._balance
+	}
+
+	deposit(amount) {
+		this._balance += amount
+	}
+	withdraw(amount) {
+		this._balance -= amount
+		this._checkMinimumBalance()
+	}
 }
+
+const account1 = new BankAccount('1234567890', 500)
+
+account1.deposit(200)
+account1.withdraw(610)
+
+const result = account1.getBalance()
+console.log(result)
