b760f44a89c44b092f1c85a19a10e7b6b6c7b6ae
Monika <codingbymon@gmail.com>
2024-07-02 11:25:15 +0200

    private field

--- script.js
+++ script.js
@@ -14,4 +14,29 @@ class TaskList {
 
 		this.#tasks.push(task)
 	}
+
+	removeTask(title) {
+		const index = this.#tasks.findIndex(obj => {
+			return obj.title === title
+		})
+		if (index !== -1) {
+			this.#tasks.splice(index, 1)
+		}
+	}
+
+	markTaskAsDone(title) {
+		const index = this.#tasks.findIndex(obj => obj.title === title)
+
+		this.#tasks.at(index).status = 'finish'
+		console.log(this.#tasks)
+
+		// zrobić to samo za pomocą metody find()
+	}
 }
+
+const test = new TaskList()
+test.addTask('hej')
+test.addTask('ho')
+test.addTask('hu')
+test.markTaskAsDone('ho')
+test.removeTask('hu')
