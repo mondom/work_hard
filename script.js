b8e1e8731a95424b68227b7fc3d6ed81852e191f
Monika <codingbymon@gmail.com>
2024-07-03 17:34:59 +0200

    try & catch & finally

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 'use strict'
 
 class Product {
-	constructor(name, price) {
+	constructor(name, price, quantity) {
 		this.name = name
 		this.price = price
+		this.quantity = quantity
 	}
 
-	calculateDiscount(discountPercentage) {
-		if (discountPercentage < 0 || discountPercentage > 100) {
-			throw new Error('Niewłaściwy procent zniżki')
+	buy(amount) {
+		if (amount > this.quantity) {
+			throw new Error('Nie ma wystarczajacej ilosci produktow na stanie.')
+		} else if (amount <= this.quantity) {
+			this.quantity = this.quantity - amount
+			console.log('Zakup został sfinalizowany');
 		}
-		const discountAmount = (discountPercentage / 100) * this.price
-		const discountedPrice = this.price - discountAmount
-		console.log(discountedPrice)
-		return discountedPrice
 	}
 }
 
-const product = new Product('Telewizor', 2000)
-
 try {
-	const test = product.calculateDiscount(20)
-	console.log(`Przed znizka: ${product.price}`)
-	console.log(`Po znizce (20%): ${test}`)
+	const product = new Product('Telefon', 999, 10)
+	product.buy(10)
 } catch (error) {
 	console.error(error.message)
+} finally {
+	console.log('Obsluga bledu zostala zakonczona.')
 }
