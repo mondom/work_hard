0f5fdd02539bc35d65cc41543b5bdc0d5cd0ed09
Monika <codingbymon@gmail.com>
2024-07-03 17:16:01 +0200

    try & catch

--- script.js
+++ script.js
@@ -10,11 +10,19 @@ class Product {
 		if (discountPercentage < 0 || discountPercentage > 100) {
 			throw new Error('Niewłaściwy procent zniżki')
 		}
-		const discountAmount = discountPercentage * this.price
+		const discountAmount = (discountPercentage / 100) * this.price
 		const discountedPrice = this.price - discountAmount
+		console.log(discountedPrice)
 		return discountedPrice
 	}
 }
 
-const product = new Product()
+const product = new Product('Telewizor', 2000)
 
+try {
+	const test = product.calculateDiscount(20)
+	console.log(`Przed znizka: ${product.price}`)
+	console.log(`Po znizce (20%): ${test}`)
+} catch (error) {
+	console.error(error.message)
+}
