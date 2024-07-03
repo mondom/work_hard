6a0156c6dc9fa1a020527e57b682f1624269b8df
Monika <codingbymon@gmail.com>
2024-07-03 14:40:37 +0200

    try & catch

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function divideNumbers(a, b) {
 	try {
 		if (b === 0) {
-			throw new Error('Nie mozna dzielic przez zero!')
+			throw new Error('Nie można podzielić przez zero.')
 		}
 		return a / b
 	} catch (error) {
@@ -11,25 +11,5 @@ function divideNumbers(a, b) {
 	}
 }
 
-console.log(divideNumbers(10, 2))
-console.log(divideNumbers(8, 0))
-console.log(divideNumbers(12, 4))
-
-function substract(x, y) {
-	try {
-		if (x < y) {
-			throw new Error('Liczba nie może być ujemna.')
-		}
-		return x - y
-	} catch (error) {
-		console.error(error.message)
-	}
-}
-
-console.log('---------------------')
-
-console.log(substract(10, 9))
-console.log(substract(10, 12))
-
-
-
+console.log(divideNumbers(10,2));
+console.log(divideNumbers(10,0));
