9b7e998088efa79ee20f3d67967a8714deda6f2b
Monika <codingbymon@gmail.com>
2024-07-03 13:19:40 +0200

    instanceof

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
 'use strict'
 
-class Animal {
-	constructor(name, age) {
-		this.name = name
-		this.age = age
+class Vehicle {
+	constructor(brand, year, numberOfWheels) {
+		this.brand = brand
+		this.year = year
+		this.numberOfWheels = numberOfWheels
 	}
 
-	eat(){
-		console.log(`Zwierzę ${this.name} je.`);
+	honk() {
+		console.log('Dzwiek klaksonu: Beep beep!')
 	}
-}
-
-class Cat extends Animal {
-	constructor(name,age){
-		super(name,age)
-	}
-	meow(){
-		console.log(`Kot ${this.name} miauczy.`);
-	}
-}
-class Dog extends Animal {
-	constructor(name,age){
-		super(name,age)
-	}
-	bark(){
-		console.log(`Pies ${this.name} szczeka.`);
+	getAge() {
+		const currentDate = new Date()
+		const currentYear = currentDate.getFullYear()
+		return currentYear - this.year
+	
 	}
 }
 
+class Car extends Vehicle {
+	constructor(brand, year, numberOfWheels, engineCapacity) {
+		super(brand, year, numberOfWheels)
+		this.engineCapacity = engineCapacity
+	}
+	startEngine() {
+		console.log('Silnik uruchomiony!')
+	}
+}
 
-const animal1 = new Animal("Rex", 3);
-const cat1 = new Cat("Mruczek", 5);
-const dog1 = new Dog("Kajtek", 2);
+class Bicycle extends Vehicle {
+	constructor(brand, year, numberOfWheels, gearCount) {
+		super(brand, year, numberOfWheels)
+		this.gearCount = gearCount
+	}
+	ringBell() {
+		console.log('Ding ding!')
+	}
+}
 
-animal1.eat()
-cat1.eat()
-cat1.meow()
-dog1.eat()
-dog1.bark()
+const vehicle = new Vehicle('Fiat', 2005, 4)
+vehicle.honk()
+console.log('Wiek pojazdu: ' + vehicle.getAge())
 
-console.log(animal1 instanceof Animal);
-console.log(cat1 instanceof Cat);
-console.log(dog1 instanceof Dog);
-console.log(animal1 instanceof Cat);
-console.log(dog1 instanceof Animal);
+
+const car = new Car("Ford", 2018, 4, "2.0L")
+car.startEngine()
+console.log('Wiek pojazdu: ' + car.getAge())
+
+const bicycle = new Bicycle("Trek", 2020, 2, 7);
+bicycle.ringBell()
+console.log(`Wiek pojazdu ${bicycle.getAge()}`);
+
+console.log("Czy car jest instancja Vehicle?", car instanceof Vehicle);
+
+console.log("Czy bicycle jest instancja Vehicle?", bicycle instanceof Vehicle);
