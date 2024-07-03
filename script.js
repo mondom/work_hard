cef72c210884df637d9c32812ab2ca2ca202746e
Monika <codingbymon@gmail.com>
2024-07-03 17:56:44 +0200

    callback

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
 'use strict'
 
-class Product {
-	constructor(name, price, quantity) {
-		this.name = name
-		this.price = price
-		this.quantity = quantity
-	}
-
-	buy(amount) {
-		if (amount > this.quantity) {
-			throw new Error('Nie ma wystarczajacej ilosci produktow na stanie.')
-		} else if (amount <= this.quantity) {
-			this.quantity = this.quantity - amount
-			console.log('Zakup został sfinalizowany');
-		}
-	}
+function fetchData(callback) {
+	setTimeout(function () {
+		const data = "Witaj, swiecie!";
+		callback(data);
+	}, 2000);
 }
 
-try {
-	const product = new Product('Telefon', 999, 10)
-	product.buy(10)
-} catch (error) {
-	console.error(error.message)
-} finally {
-	console.log('Obsluga bledu zostala zakonczona.')
-}
+fetchData(function (response) {
+	console.log(response);
+});
