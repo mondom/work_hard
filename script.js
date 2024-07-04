8c095e0e81e385e1ce449465bcd4f5f68a497689
Monika <codingbymon@gmail.com>
2024-07-04 11:35:35 +0200

    test

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
 'use strict'
 
-function getUsersFromDatabase(callback) {
-	setTimeout(() => {
-		const users = [
-			{ id: 1, name: 'Jan Kowalski' },
-			{ id: 2, name: 'Anna Nowak' },
-			{ id: 3, name: 'Piotr Wisniewski' },
-		]
-		callback(users)
-	}, 2000)
-}
+const foo = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve('bar');
+    }, 3000);
+  });
 
-function getEmail(userID, callback) {
-	setTimeout(() => {
-		let email = userID + '@example.com'
-		callback(email)
-	}, 2000)
-}
+  console.log(foo);
+  console.log(foo);
 
-function mainFunction() {
-	getUsersFromDatabase(users => {
-		console.log(users)
-		users.forEach(user => {
-			getEmail(user.id, email => {
-				console.log(`Uzytkownik ${user.name} ma adres e-mail: ${email}`)
-			})
-		})
-	})
-}
 
-mainFunction()
+
+  
+  
