48edabd149b35cb7d1c797b6b6247b1ae27daa51
Monika <codingbymon@gmail.com>
2024-07-05 11:20:59 +0200

    arr clg

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 'use strict'
 
-function fetchData() {
-	return new Promise(resolve => {
-		setTimeout(() => {
-			const data = [1, 2, 3, 4, 5]
-			resolve(data)
-		}, 800)
-	})
-}
+// function fetchData() {
+// 	return new Promise(resolve => {
+// 		setTimeout(() => {
+// 			const data = [1, 2, 3, 4, 5]
+// 			resolve(data)
+// 		}, 800)
 
-const data = fetchData()
+// 	})
+// }
 
-data
-	.then(dat => console.log(`Dane z serwera: ${dat}`))
+// fetchData()
+// 	.then(dat => console.log(`Dane z serwera: `, dat))
 
-	.catch(err => console.error('Blad podczas pobierania danych: ' + err))
+// 	.catch(err => console.error('Blad podczas pobierania danych: ' + err))
 
+const arr = [1, 2, 3, 4, 5]
 
-  const arr = [9,8,7,6,5]
-  console.log(arr);
+console.log(arr)
+console.log('I to również: ', arr)
+
+console.log(`To jest tablica: ${arr}`)
+console.log('To też jest tablica: ' + arr)
+
+console.log('A tutaj łączymy metodą tablicę: ' + arr.join(' '))
