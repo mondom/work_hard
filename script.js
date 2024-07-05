2638aa94d26c2d5bbcd920dc43ff880098de4738
Monika <codingbymon@gmail.com>
2024-07-05 10:49:40 +0200

    promise

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ function fetchData() {
 		setTimeout(() => {
 			const data = [1, 2, 3, 4, 5]
 			resolve(data)
-		},800)
+		}, 800)
 	})
 }
 
 const data = fetchData()
 
 data
-	.then(dat => console.log('Dane z serwera: ' + dat))
+	.then(dat => console.log(`Dane z serwera: ${dat}`))
+
 	.catch(err => console.error('Blad podczas pobierania danych: ' + err))
+
+
+  const arr = [9,8,7,6,5]
+  console.log(arr);
