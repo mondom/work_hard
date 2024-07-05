53330a606f0d6ae532d2947d7e4ecf4ad1c15a42
Monika <codingbymon@gmail.com>
2024-07-05 12:10:45 +0200

    promise chaining

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
 'use strict'
 
-// function fetchData() {
-// 	return new Promise(resolve => {
-// 		setTimeout(() => {
-// 			const data = [1, 2, 3, 4, 5]
-// 			resolve(data)
-// 		}, 800)
+function fetchData() {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          const success = true; // Zmień na false, aby przetestować scenariusz błędu
+          if (success) {
+              console.log('Dane pobrane');
+              resolve([1, 2, 3, 4, 5]);
+          } else {
+              reject(new Error('Błąd podczas pobierania danych'));
+          }
+      }, 1000);
+  });
+}
 
-// 	})
-// }
+function processData(data) {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          const success = true; // Zmień na false, aby przetestować scenariusz błędu
+          if (success) {
+              console.log('Dane przetworzone');
+              resolve(data.map(item => item * 2));
+          } else {
+              reject(new Error('Błąd podczas przetwarzania danych'));
+          }
+      }, 1000);
+  });
+}
 
-// fetchData()
-// 	.then(dat => console.log(`Dane z serwera: `, dat))
+function displayData(data) {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          const success = true; // Zmień na false, aby przetestować scenariusz błędu
+          if (success) {
+              console.log('Dane wyświetlone:', data);
+              resolve();
+          } else {
+              reject(new Error('Błąd podczas wyświetlania danych'));
+          }
+      }, 1000);
+  });
+}
 
-// 	.catch(err => console.error('Blad podczas pobierania danych: ' + err))
-
-const arr = [1, 2, 3, 4, 5]
-
-console.log(arr)
-console.log('I to również: ', arr)
-
-console.log(`To jest tablica: ${arr}`)
-console.log('To też jest tablica: ' + arr)
-
-console.log('A tutaj łączymy metodą tablicę: ' + arr.join(' '))
+// Promise Chaining z obsługą błędów
+fetchData()
+  .then(data => {
+      return processData(data);
+  })
+  .then(processedData => {
+      return displayData(processedData);
+  })
+  .then(() => {
+      console.log('Wszystkie operacje zakończone pomyślnie');
+  })
+  .catch(error => {
+      console.error('Błąd:', error.message);
+  });
