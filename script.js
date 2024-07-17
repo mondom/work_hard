eeea758fa58d669703b446fbc4ff0716c1d93375
Monika <codingbymon@gmail.com>
2024-07-17 13:06:25 +0200

    learning

--- script.js
+++ script.js
@@ -1,146 +1,43 @@
 "use strict"
 
-// function fetchData() {
-//   return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//           const success = true; // Zmień na false, aby przetestować scenariusz błędu
-//           if (success) {
-//               console.log('Dane pobrane');
-//               resolve([1, 2, 3, 4, 5]);
-//           } else {
-//               reject(new Error('Błąd podczas pobierania danych'));
-//           }
-//       }, 1000);
-//   });
-// }
-
-// function processData(data) {
-//   return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//           const success = true; // Zmień na false, aby przetestować scenariusz błędu
-//           if (success) {
-//               console.log('Dane przetworzone');
-//               resolve(data.map(item => item * 2));
-//           } else {
-//               reject(new Error('Błąd podczas przetwarzania danych'));
-//           }
-//       }, 1000);
-//   });
-// }
-
-// function displayData(data) {
-//   return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//           const success = true; // Zmień na false, aby przetestować scenariusz błędu
-//           if (success) {
-//               console.log('Dane wyświetlone:', data);
-//               resolve();
-//           } else {
-//               reject(new Error('Błąd podczas wyświetlania danych'));
-//           }
-//       }, 1000);
-//   });
-// }
-
-// // Promise Chaining z obsługą błędów
-// fetchData()
-//   .then(data => {
-//       return processData(data);
-//   })
-//   .then(processedData => {
-//       return displayData(processedData);
-//   })
-//   .then(() => {
-//       console.log('Wszystkie operacje zakończone pomyślnie');
-//   })
-//   .catch(error => {
-//       console.error('Błąd:', error.message);
-//   });
-
-// function fetchNumbers() {
-// 	return new Promise((resolve, reject) => {
-// 		setTimeout(() => {
-// 			console.log('Podaję liczby do analizy')
-// 			resolve([2, 4, 6])
-// 		}, 1000)
-// 	})
-// }
-
-// function count(numbers) {
-// 	return new Promise((resolve, reject) => {
-// 		setTimeout(() => {
-// 			console.log('Wykonuję obliczenia')
-// 			resolve(
-// 				numbers.reduce((acc, curr) => {
-// 					return acc + curr
-// 				})
-// 			)
-// 		}, 1000)
-// 	})
-// }
-// function showResult(result) {
-// 	return new Promise((resolve, reject) => {
-// 		setTimeout(() => {
-// 			if (result >= 0 && result <= 12) {
-// 				resolve(
-// 					console.log(`Otrzymany wynik to: ${result}`)
-// 				)
-
-// 			} else {
-// 				reject(new Error('Wynik jest nieprawidłowy - zmień zakres.'))
-// 			}
-// 		}, 1000)
-// 	})
-// }
-
-// fetchNumbers()
-// .then(num => {
-// 	return count(num)
-// }).then(res => {
-//   return showResult(res)
-// }).catch(err => {
-//   console.error(err);
-// })
-
-const fetchNumer = () => {
-	return new Promise((resolve, reject) => {
+function getMenu() {
+	return new Promise(resolve => {
 		setTimeout(() => {
-			let data = [1, 2, 3, 4, 5, 6]
-			if (data !== undefined) {
-				console.log("Pobieranie danych do analizy")
-				resolve(data)
-			} else {
-				reject(console.log("Nie pobrano żadnych danych"))
-			}
+			const menu = [
+				{ name: "Pizza Margherita", price: 20 },
+				{ name: "Spaghetti Bolognese", price: 15 },
+				{ name: "Caesar Salad", price: 10 },
+			]
+			resolve(menu)
 		}, 1000)
 	})
 }
 
-const processingData = data => {
+function selectDish(menu, index) {
 	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			console.log("Przetwarzam pobrane dane")
-			resolve(
-				data.reduce((acc, curr) => {
-					return acc += curr
-				})
-			)
-		})
-	})
-}
-
-const showResult = result => {
-	return new Promise((resolve, reject) => {
-		if (result !== undefined) {
-			resolve(console.log(result))
+		if (index <= menu.length && index >= 0) {
+			resolve(menu[index])
+			
 		} else {
-			reject(console.log("Wystąpił nieoczekiwany błąd"))
+			reject("Nieprawidlowy indeks dania")
 		}
 	})
 }
 
-fetchNumer().then(data => {
-	return processingData(data)
-}).then(result => {
-    return showResult(result)
-})
+function calculateTotal(selectedDishes) {
+	return new Promise(resolve => {
+		const total = selectedDishes.reduce((acc, dish) => {
+			return (acc += dish.price)
+		})
+		resolve(total)
+	})
+}
+
+getMenu()
+	.then((menu) => {
+
+		return selectDish(menu, 0)
+	})
+	.then(result => {
+		return calculateTotal([result])
+	})
