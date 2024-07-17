24d1cdb0b145b66ef5035ea418e1bcfc0817e4b9
Monika <codingbymon@gmail.com>
2024-07-17 13:38:51 +0200

    learning

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ function selectDish(menu, index) {
 	return new Promise((resolve, reject) => {
 		if (index <= menu.length && index >= 0) {
 			resolve(menu[index])
-			
 		} else {
 			reject("Nieprawidlowy indeks dania")
 		}
@@ -27,17 +26,31 @@ function selectDish(menu, index) {
 function calculateTotal(selectedDishes) {
 	return new Promise(resolve => {
 		const total = selectedDishes.reduce((acc, dish) => {
-			return (acc += dish.price)
-		})
+			return acc + dish.price
+		}, 0)
+
 		resolve(total)
 	})
 }
 
-getMenu()
-	.then((menu) => {
+function confirmOrder(total) {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve(
+				console.log("Zamowienie potwierdzone. Laczna kwota: " + total + " zl")
+			)
+		}, 1000)
+	})
+}
 
+getMenu()
+	.then(menu => {
 		return selectDish(menu, 0)
 	})
 	.then(result => {
 		return calculateTotal([result])
+	})
+	.then(tot => {
+		console.log("Laczna cena zamowienia:", tot, "zl")
+		return confirmOrder(tot)
 	})
