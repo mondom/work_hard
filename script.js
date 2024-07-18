6dab566fa1558b4c133f45f05aa58fa7e6a2c0d4
Monika <codingbymon@gmail.com>
2024-07-18 10:30:09 +0200

    finnaly finish

--- script.js
+++ script.js
@@ -3,15 +3,25 @@
 function downloadFile(URL) {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
-			const fileContent = "Tekstowa zawartość pliku"
+			const fileContent = URL
 			const success = true
 
 			if (success === true) {
 				resolve(fileContent)
 			} else if (success === false) {
-				console.error("Błąd podczas pobierania pliku")
-				reject("Błąd podczas wywołania danych")
+				console.error("fuck")
+				reject("Błąd podczas pobierania pliku")
 			}
 		}, 2000)
-	})
+	}).finally(() => console.log("Pobieranie zakończone"))
 }
+const URL =
+	"https://cdn.pixabay.com/photo/2023/08/19/13/42/flowers-8200510_1280.jpg"
+
+downloadFile(URL)
+	.then(content => {
+		console.log("Zawartosc pliku:", content)
+	})
+	.catch(error => {
+		console.log("Blad:", error)
+	})
