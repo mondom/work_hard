fbf98b5c63de3eb24c869bd9796a5859ea06f3a9
Monika <codingbymon@gmail.com>
2024-07-19 10:26:24 +0200

    next task

--- script.js
+++ script.js
@@ -1,61 +1,43 @@
 "use strict"
 
-// function fetchUserData() {
-// 	return new Promise((resolve, reject) => {
-// 		setTimeout(() => {
-// 			const userData = { name: "John", age: 30 }
-// 			resolve(userData)
-// 		}, 1000)
-// 	})
-// }
-// function fetchOrderData() {
-// 	return new Promise((resolve, reject) => {
-// 		setTimeout(() => {
-// 			const orderData = { id: 123, total: 50 }
-// 			resolve(orderData)
-// 		}, 1500)
-// 	})
-// }
-// function fetchProductData() {
-// 	return new Promise((resolve, reject) => {
-// 		setTimeout(() => {
-// 			const productData = { name: "Phone", price: 500 }
-// 			resolve(productData)
-// 		}, 1500)
-// 	})
-// }
-
-// Promise.allSettled([
-// 	fetchUserData(),
-// 	fetchOrderData(),
-// 	fetchProductData(),
-// ]).then(results => {
-// 	results.forEach((result, index) => {
-// 		if (result.status === "fulfilled") {
-// 			console.log(`Wynik dla obietnicy nr ${index + 1} jest poprawny:`)
-// 			Object.entries(result.value).forEach(([key, value]) => {
-// 				console.log(`${key}: ${value}`)
-// 			})
-// 		} else if (result.status === "rejected") {
-// 			console.log(`Wynik dla obietnicy nr ${index + 1} jest nieprawidłowy: ${result.reason}`)
-// 		}
-// 	})
-// })
-
-// const user = {
-// 	name: 'John',
-// 	age: 30,
-// 	city: 'New York'
-//   };
-
-//   const entries = Object.entries(user);
-//   console.log(entries);
-
-const person = {
-	name: "Alice",
-	age: 25,
+function fetchUserData() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const userData = { name: "John", age: 30 }
+			resolve(userData)
+		})
+	})
 }
-const test = Object.entries(person).forEach(([key,value])=>{
-  console.log(key, value);
-})
+function fetchOrderData() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const orderData = { id: 123, total: 50 };
+			if(!Array.isArray(orderData)){
+        resolve(orderData)
+      } else{
+        reject('Błąd serwera')
+      }
+		})
+	})
+}
+function fetchProductData() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const productData = { name: "Phone", price: 500 }
+			resolve(productData)
+		})
+	})
+}
 
+Promise.allSettled([fetchUserData(), fetchOrderData(), fetchProductData()]).then(resultsArr => {
+  resultsArr.forEach(result => {
+    if(result.status === 'fulfilled'){
+      console.log(result.value);
+    } else if(result.status === 'rejected'){
+      console.log(`Nie udało się pobrać danych użytkownika: ${result.reason}`);
+    }
+  })
+ 
+}).catch(err => {
+  console.log(`Wystąpił błąd: ${err}`);
+})
