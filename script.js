eb35a05bcdb024209d07c4b2cd3f3752a448f176
Monika <codingbymon@gmail.com>
2024-07-19 11:37:02 +0200

    finish task

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function fetchUserData() {
 function fetchOrderData() {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
-			const orderData = { id: 123, total: 50 };
-			if(!Array.isArray(orderData)){
-        resolve(orderData)
-      } else{
-        reject('Błąd serwera')
-      }
+			const orderData = { id: 123, total: 50 }
+			if (!Array.isArray(orderData)) {
+				resolve(orderData)
+			} else {
+				reject("Błąd serwera")
+			}
 		})
 	})
 }
@@ -29,15 +29,37 @@ function fetchProductData() {
 	})
 }
 
-Promise.allSettled([fetchUserData(), fetchOrderData(), fetchProductData()]).then(resultsArr => {
-  resultsArr.forEach(result => {
-    if(result.status === 'fulfilled'){
-      console.log(result.value);
-    } else if(result.status === 'rejected'){
-      console.log(`Nie udało się pobrać danych użytkownika: ${result.reason}`);
-    }
-  })
- 
-}).catch(err => {
-  console.log(`Wystąpił błąd: ${err}`);
-})
+Promise.allSettled([fetchUserData(), fetchOrderData(), fetchProductData()])
+	.then(resultsArr => {
+		const userDataResult = resultsArr[0]
+		const orderDataResult = resultsArr[1]
+		const productDataResult = resultsArr[2]
+
+		if (userDataResult.status === "fulfilled") {
+			console.log("Dane uzytkownika:", userDataResult.value)
+		} else {
+			console.log(
+				"Nie udalo sie pobrac danych uzytkownika:",
+				userDataResult.reason
+			)
+		}
+		if (orderDataResult.status === "fulfilled") {
+			console.log("Dane zamówienia:", orderDataResult.value)
+		} else {
+			console.log(
+				"Nie udalo sie pobrac danych zamówienia:",
+				orderDataResult.reason
+			)
+		}
+		if (productDataResult.status === "fulfilled") {
+			console.log("Dane produktu:", productDataResult.value)
+		} else {
+			console.log(
+				"Nie udalo sie pobrac danych produktu:",
+				productDataResult.reason
+			)
+		}
+	})
+	.catch(err => {
+		console.log(`Wystąpił błąd: ${err}`)
+	})
