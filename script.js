b1b93399e5f7fb4a931606ed94d5d5a17935324c
Monika <codingbymon@gmail.com>
2024-07-22 14:37:07 +0200

    comparison of promise and async/await

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
 "use strict"
 
-const checkAge = age => {
-    return new Promise((resolve, reject) => {
-        if (age >= 18) {
-            resolve()
-        } else {
-            reject('Masz za mało lat')
-        }
-    })
-}
+function fetchData() {
+	return new Promise((resolve, reject) => {
+	  setTimeout(() => {
+		resolve("Dane zostały pobrane");
+	  }, 1000);
+	});
+  }
+  
+  function anotherFetch() {
+	return new Promise((resolve, reject) => {
+	  setTimeout(() => {
+		resolve("Dodatkowe dane zostały pobrane");
+	  }, 1000);
+	});
+  }
+  
+  fetchData()
+	.then(data => {
+	  console.log(data);
+	  return anotherFetch();
+	})
+	.then(moreData => {
+	  console.log(moreData);
+	})
+	.catch(error => {
+	  console.error("Wystąpił błąd:", error);
+	});
 
-const doubleCheck = () => {
-    return new Promise((resolve, reject) => {
-        console.log('Sprawdzam jeszcze raz...')
-        setTimeout(() => {
-            resolve('Faktycznie, wiek się zgadza.')
-        }, 1000)
-    })
-}
 
-checkAge(50)
-    .then(() => {
-        console.log('Chyba możesz wejść...')
-        return doubleCheck()
-    })
-    .then(res => console.log(res))
-    .then(() => {
-        console.log('Sprawdzanie zakończone!')
-    })
-    .catch(error => console.error(error))
+	async function fetchData() {
+		return new Promise((resolve, reject) => {
+		  setTimeout(() => {
+			resolve("Dane zostały pobrane");
+		  }, 1000);
+		});
+	  }
+	  
+	  async function anotherFetch() {
+		return new Promise((resolve, reject) => {
+		  setTimeout(() => {
+			resolve("Dodatkowe dane zostały pobrane");
+		  }, 1000);
+		});
+	  }
+	  
+	  async function fetchAllData() {
+		try {
+		  const data = await fetchData();
+		  console.log(data);
+		  const moreData = await anotherFetch();
+		  console.log(moreData);
+		} catch (error) {
+		  console.error("Wystąpił błąd:", error);
+		}
+	  }
+	  
+	  fetchAllData();
