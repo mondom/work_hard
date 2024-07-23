4e0812fa00bb6cdfda416e1bb5bc696feae19717
Monika <codingbymon@gmail.com>
2024-07-23 11:08:20 +0200

    notation of the async function

--- script.js
+++ script.js
@@ -1,59 +1,7 @@
 "use strict"
 
-function fetchData() {
-	return new Promise((resolve, reject) => {
-	  setTimeout(() => {
-		resolve("Dane zostały pobrane");
-	  }, 1000);
-	});
-  }
-  
-  function anotherFetch() {
-	return new Promise((resolve, reject) => {
-	  setTimeout(() => {
-		resolve("Dodatkowe dane zostały pobrane");
-	  }, 1000);
-	});
-  }
-  
-  fetchData()
-	.then(data => {
-	  console.log(data);
-	  return anotherFetch();
-	})
-	.then(moreData => {
-	  console.log(moreData);
-	})
-	.catch(error => {
-	  console.error("Wystąpił błąd:", error);
-	});
+async function test(){}
+
+const test = async(params)=>{}
 
 
-	async function fetchData() {
-		return new Promise((resolve, reject) => {
-		  setTimeout(() => {
-			resolve("Dane zostały pobrane");
-		  }, 1000);
-		});
-	  }
-	  
-	  async function anotherFetch() {
-		return new Promise((resolve, reject) => {
-		  setTimeout(() => {
-			resolve("Dodatkowe dane zostały pobrane");
-		  }, 1000);
-		});
-	  }
-	  
-	  async function fetchAllData() {
-		try {
-		  const data = await fetchData();
-		  console.log(data);
-		  const moreData = await anotherFetch();
-		  console.log(moreData);
-		} catch (error) {
-		  console.log("Wystąpił błąd:", error);
-		}
-	  }
-	  
-	  fetchAllData();
