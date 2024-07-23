d19383aef87ba7d303516e296f2e92672c2d3d1f
Monika <codingbymon@gmail.com>
2024-07-23 18:33:12 +0200

    next task

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 "use strict"
 
-function delay(ms){
-    return new Promise(resolve => {
-        setTimeout(()=>{
-            resolve()
-        },ms)
-    })
+function delay(ms) {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve()
+		}, ms)
+	})
 }
 
 const tasks = [
-    "Przygotuj sniadanie",
-    "Umyj naczynia",
-    "Posprzataj pokoj",
-    "Obejrzyj ulubiony film",
-    "Przeczytaj ksiazke",
-];
+	"Przygotuj sniadanie",
+	"Umyj naczynia",
+	"Posprzataj pokoj",
+	"Obejrzyj ulubiony film",
+	"Przeczytaj ksiazke",
+]
+
+async function processTasks() {
+	for (const task of tasks) {
+        await delay(1000)
+		console.log(`Rozpoczynam zadanie: ${task}`)
+		await delay(2000)
+		console.log(`Zadanie zakonczone: ${task}`)
+	}
+}
+
+processTasks()
