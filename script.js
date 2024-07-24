1a75db951701688f8f5052c13265dacb871ea981
Monika <codingbymon@gmail.com>
2024-07-24 14:20:25 +0200

    generator

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
 "use strict"
 
-function compressFile(file) {
-	return new Promise(resolve => {
-		const randomDelay = Math.floor(Math.random() * 2000) + 1000
-		setTimeout(() => resolve("Skompresowano plik: " + file), randomDelay)
-	})
+function* randomNumberGenerator() {
+    while (true) {
+        yield Math.floor(Math.random()*10);
+    }
 }
 
-const filesToCompress = [
-	"obrazek.jpg",
-	"dokument.docx",
-	"muzyka.mp3",
-	"video.mp4",
-	"index.html",
-]
+const generator = randomNumberGenerator();
 
-async function compressFiles() {
-	const compressFileArr = []
 
-	for (const file of filesToCompress) {
-		const result = await compressFile(file)
-		compressFileArr.push(file)
-		console.log(result)
-	}
-	return compressFileArr
+
+for (let i = 0; i < 5; i++) {
+    console.log(generator.next());
 }
-
-compressFiles()
-	.then(result => {
-		console.log("----------------")
-		console.log("Proces kompresji zakonczony.")
-		console.log("Skompresowane pliki:")
-		console.log(result)
-	})
-	.catch(err => {
-		console.error("Wystąpił błąd podczas kompresji plików:", err)
-	})
 
 
