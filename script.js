6fc62a25490a1b7fa5c937df8a55f93d39818a7c
Monika <codingbymon@gmail.com>
2024-07-24 12:55:23 +0200

    async/await

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 "use strict"
 
-function test(){}
+function compressFile(file) {
+	return new Promise(resolve => {
+		const randomDelay = Math.floor(Math.random() * 2000) + 1000
+		setTimeout(() => resolve("Skompresowano plik: " + file), randomDelay)
+	})
+}
 
-(async () => {
-    //próbuję wykonać jakieś operacje
-    try {
-        const request = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-        const json = await request.json();
-        console.log(json)
-    //jeżeli się nie uda, zwracam błąd
-    } catch(err) {
-        console.log(err);
-    }
-})();
+const filesToCompress = [
+	"obrazek.jpg",
+	"dokument.docx",
+	"muzyka.mp3",
+	"video.mp4",
+	"index.html",
+]
+
+async function compressFiles() {
+	const compressFileArr = []
+
+	for (const file of filesToCompress) {
+		const result = await compressFile(file)
+		compressFileArr.push(file)
+		console.log(result)
+	}
+	return compressFileArr
+}
+
+compressFiles()
+	.then(result => {
+		console.log("----------------")
+		console.log("Proces kompresji zakonczony.")
+		console.log("Skompresowane pliki:")
+		console.log(result)
+	})
+	.catch(err => {
+		console.error("Wystąpił błąd podczas kompresji plików:", err)
+	})
 
 
