1feb075e6dcb4e049ad8cf15025b82a18724da77
Monika <codingbymon@gmail.com>
2024-07-25 09:56:10 +0200

    generator

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 "use strict"
 
-function* productCodeGenerator(length, prefix = ''){
-    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    let codeLength = length - prefix.length
-    while(true){
-        let code = prefix
-    }
+function* productCodeGenerator(length, prefix = "") {
+	let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	let codeLength = length - prefix.length
+	while (true) {
+		let code = prefix
+		for (let i = 0; i < codeLength; i++) {
+			const randomIndex = Math.floor(Math.random() * characters.length)
+			code += characters.charAt(randomIndex)
+		}
+
+		yield code
+	}
 }
 
+const generator = productCodeGenerator(8,'LED')
+
+console.log(generator.next().value)
+console.log(generator.next().value)
+console.log(generator.next().value)
