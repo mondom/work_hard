89d8323f74ce036cc73b34e7d27af394dc306ecd
Monika <codingbymon@gmail.com>
2024-07-25 14:04:56 +0200

    generator

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ function* lotteryNumberGenerator() {
 
 let generator = lotteryNumberGenerator()
 
-for (let number of generator) {
-	console.log(number);
-}
+// for (let number of generator) {
+// 	console.log(number);
+// }
+
+// console.log(generator.next().value); // Wywołuje generator i zwraca pierwszą wartość
+// console.log(generator.next().value); // Wywołuje generator i zwraca drugą wartość
+// console.log(generator.next().value); // Wywołuje generator i zwraca trzecią wartość
+// console.log(generator.next().value); // Wywołuje generator i zwraca czwartą wartość
+// console.log(generator.next().value); // Wywołuje generator i zwraca piątą wartość
+// console.log(generator.next().done);  // Zwraca true, ponieważ generator zakończył działanie
+
+
+// let numbers = [...generator]; // Rozwija wartości generatora do tablicy
+
+// console.log(numbers); // Wypisuje tablicę z wygenerowanymi liczbami
+
+let [first, second, third, fourth, fifth] = generator;
+
+console.log(first, second, third, fourth, fifth); // Wypisuje pięć wygenerowanych liczb
+
+
