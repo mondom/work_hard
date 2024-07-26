b1136fb095eb852ce9481b060bd341dae5f0e66d
Monika <codingbymon@gmail.com>
2024-07-26 10:06:52 +0200

    generator

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ class Order {
 	}
 }
 
+function* orderGenerator(){
+	const products = [
+		{ productName: "Produkt A", quantity: 3, unitPrice: 10 },
+		{ productName: "Produkt B", quantity: 2, unitPrice: 20 },
+		{ productName: "Produkt C", quantity: 1, unitPrice: 15 },
+	];
+
+	for(const product of products){
+		const order = new Order(product.productName,product.quantity,product.unitPrice)
+		yield order
+	}
+}
+
+const results = orderGenerator()
+
+for(const result of results){
+	console.log('Produkt: ' + result.productName);
+	console.log('Ilość: ' + result.quantity);
+	console.log('Cena jednostkowa: ' + result.unitPrice);
+	console.log('Data zamówienia: ' + result.orderDate.toISOString())
+	console.log('Cena całkowita: ' + result.getTotalPrice());
+	console.log('--------------');
+}
