4cff71c11d15ee8708a16d05ec454d033f60f324
Monika <codingbymon@gmail.com>
2024-07-26 13:27:22 +0200

    generator - asynchronous iteration

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
 "use strict"
 
-function* zadania() {
+// function sleep(ms) {
+// 	return new Promise(resolve => setTimeout(resolve, ms))
+// }
+
+// async function* zadania() {
+// 	const listaZadan = [
+// 		{
+// 			id: 1,
+// 			opis: "Przygotowanie raportu",
+// 			status: "W trakcie",
+// 		},
+// 		{
+// 			id: 2,
+// 			opis: "Sprawdzenie wiadomosci",
+// 			status: "Zakonczone",
+// 		},
+// 		{
+// 			id: 3,
+// 			opis: "Spotkanie z klientem",
+// 			status: "W trakcie",
+// 		},
+// 	]
+
+// 	for (const zadanie of listaZadan) {
+// 		await sleep(1000)
+// console.log('.....', zadanie);
+// 		yield zadanie
+// 	}
+// }
+
+// const generatorZadan = zadania();
+
+// (async () => {
+// 	for await (const zadanie of generatorZadan) {
+// 		console.log("ID:", zadanie.id)
+// 		console.log("Opis:", zadanie.opis)
+// 		console.log("Status:", zadanie.status)
+// 		console.log("--------------------")
+// 	}
+// })()
+
+function wait(ms) {
+	return new Promise(resolve => {
+		setTimeout(resolve, ms)
+	})
+}
+
+async function* zadania() {
 	const listaZadan = [
 		{
 			id: 1,
@@ -19,9 +66,19 @@ function* zadania() {
 		},
 	]
 
-	for(const zadanie of listaZadan){
-		setTimeout(()=>{
-			
-		})
+	for (const zadanie of listaZadan) {
+		await wait(1000)
+		yield zadanie
 	}
 }
+
+const generatorZadan = zadania()
+
+;(async () => {
+	for await (const zadanie of generatorZadan) {
+		console.log("ID: ", zadanie.id)
+		console.log("Opis: ", zadanie.opis)
+		console.log("Status: ", zadanie.status)
+		console.log("------------")
+	}
+})()
