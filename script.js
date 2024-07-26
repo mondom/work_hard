a6221561d50ac975ed8cbe7c1e8d5b95b0fa9fd3
Monika <codingbymon@gmail.com>
2024-07-26 13:50:07 +0200

    generator - asynchronous iteration

--- script.js
+++ script.js
@@ -1,84 +1,29 @@
 "use strict"
 
-// function sleep(ms) {
-// 	return new Promise(resolve => setTimeout(resolve, ms))
-// }
-
-// async function* zadania() {
-// 	const listaZadan = [
-// 		{
-// 			id: 1,
-// 			opis: "Przygotowanie raportu",
-// 			status: "W trakcie",
-// 		},
-// 		{
-// 			id: 2,
-// 			opis: "Sprawdzenie wiadomosci",
-// 			status: "Zakonczone",
-// 		},
-// 		{
-// 			id: 3,
-// 			opis: "Spotkanie z klientem",
-// 			status: "W trakcie",
-// 		},
-// 	]
-
-// 	for (const zadanie of listaZadan) {
-// 		await sleep(1000)
-// console.log('.....', zadanie);
-// 		yield zadanie
-// 	}
-// }
-
-// const generatorZadan = zadania();
-
-// (async () => {
-// 	for await (const zadanie of generatorZadan) {
-// 		console.log("ID:", zadanie.id)
-// 		console.log("Opis:", zadanie.opis)
-// 		console.log("Status:", zadanie.status)
-// 		console.log("--------------------")
-// 	}
-// })()
-
-function wait(ms) {
+function sleep(ms) {
 	return new Promise(resolve => {
 		setTimeout(resolve, ms)
 	})
 }
 
-async function* zadania() {
-	const listaZadan = [
-		{
-			id: 1,
-			opis: "Przygotowanie raportu",
-			status: "W trakcie",
-		},
-		{
-			id: 2,
-			opis: "Sprawdzenie wiadomosci",
-			status: "Zakonczone",
-		},
-		{
-			id: 3,
-			opis: "Spotkanie z klientem",
-			status: "W trakcie",
-		},
+async function* zamowieniaGenerator() {
+	const zamowienia = [
+		{ id: 1, status: "Przyjete" },
+		{ id: 2, status: "Przygotowywane" },
+		{ id: 3, status: "Gotowe" },
 	]
 
-	for (const zadanie of listaZadan) {
-		await wait(1000)
-		yield zadanie
+	for (const zamowienie of zamowienia) {
+		await sleep(1500)
+		yield zamowienie
 	}
 }
 
-const generatorZadan = zadania()
-
+const generatorZamowien = zamowieniaGenerator()
 ;(async () => {
-	for await (const zadanie of generatorZadan) {
-		console.log("ID: ", zadanie.id)
-		console.log("Opis: ", zadanie.opis)
-		console.log("Status: ", zadanie.status)
-		console.log("------------")
+	for await (const zamowienie of generatorZamowien) {
+		console.log("ID zamowienia:", zamowienie.id)
+		console.log("Status zamowienia:", zamowienie.status)
+		console.log("--------------------")
 	}
 })()
