7a7e1f49dcda6df2400a0cfe2ed8a2e1fa29f6b6
Monika <codingbymon@gmail.com>
2024-07-26 11:02:05 +0200

    generator.throw()

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
 "use strict"
 
-class Order {
-	constructor(productName, quantity, unitPrice, orderDate = new Date()) {
-		this.productName = productName
-		this.quantity = quantity
-		this.unitPrice = unitPrice
-		this.orderDate = orderDate
-	}
+function* kasyno() {
+	let sumaPunktow = 0
 
-	getTotalPrice(){
-		let totalPrice = this.quantity * this.unitPrice
-		return totalPrice
-	}
-}
-
-function* orderGenerator(){
-	const products = [
-		{ productName: "Produkt A", quantity: 3, unitPrice: 10 },
-		{ productName: "Produkt B", quantity: 2, unitPrice: 20 },
-		{ productName: "Produkt C", quantity: 1, unitPrice: 15 },
-	];
-
-	for(const product of products){
-		const order = new Order(product.productName,product.quantity,product.unitPrice)
-		yield order
+	while (true) {
+		const wynikRzutu = Math.floor(Math.random() * 6) + 1
+		sumaPunktow += wynikRzutu
+		if (sumaPunktow > 20) {
+			throw new Error("Przekroczono limit punktów!")
+		}
+		yield sumaPunktow
 	}
 }
 
-const results = orderGenerator()
+const generator = kasyno()
 
-for(const result of results){
-	console.log('Produkt: ' + result.productName);
-	console.log('Ilość: ' + result.quantity);
-	console.log('Cena jednostkowa: ' + result.unitPrice);
-	console.log('Data zamówienia: ' + result.orderDate.toISOString())
-	console.log('Cena całkowita: ' + result.getTotalPrice());
-	console.log('--------------');
+try {
+	while (true) {
+		const wynik = generator.next().value
+		console.log("Suma punktow:", wynik)
+	}
+} catch (err){
+	console.error(err)
 }
