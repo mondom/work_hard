0050f9939807b4da883c205ec83eb25ad8e6067e
Monika <codingbymon@gmail.com>
2024-07-26 09:53:18 +0200

    generator

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
 "use strict"
 
-function* lotteryNumberGenerator() {
-	const count = 5
-	for (let i = 0; i < count; i++) {
-		let randomNum = Math.floor(Math.random() * 100) + 1
-		yield randomNum
+class Order {
+	constructor(productName, quantity, unitPrice, orderDate = new Date()) {
+		this.productName = productName
+		this.quantity = quantity
+		this.unitPrice = unitPrice
+		this.orderDate = orderDate
+	}
+
+	getTotalPrice(){
+		let totalPrice = this.quantity * this.unitPrice
+		return totalPrice
 	}
 }
-
-let generator = lotteryNumberGenerator()
-
-// for (let number of generator) {
-// 	console.log(number);
-// }
-
-// console.log(generator.next().value); // Wywołuje generator i zwraca pierwszą wartość
-// console.log(generator.next().value); // Wywołuje generator i zwraca drugą wartość
-// console.log(generator.next().value); // Wywołuje generator i zwraca trzecią wartość
-// console.log(generator.next().value); // Wywołuje generator i zwraca czwartą wartość
-// console.log(generator.next().value); // Wywołuje generator i zwraca piątą wartość
-// console.log(generator.next().done);  // Zwraca true, ponieważ generator zakończył działanie
-
-
-// let numbers = [...generator]; // Rozwija wartości generatora do tablicy
-
-// console.log(numbers); // Wypisuje tablicę z wygenerowanymi liczbami
-
-let [first, second, third, fourth, fifth] = generator;
-
-console.log(first, second, third, fourth, fifth); // Wypisuje pięć wygenerowanych liczb
-
 
