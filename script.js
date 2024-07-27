89290e538f007311b9ea09aa12bf4d55d599b7fe
Monika <codingbymon@gmail.com>
2024-07-27 20:43:15 +0200

    test

--- script.js
+++ script.js
@@ -1,29 +1,2 @@
 "use strict"
 
-function sleep(ms) {
-	return new Promise(resolve => {
-		setTimeout(resolve, ms)
-	})
-}
-
-async function* zamowieniaGenerator() {
-	const zamowienia = [
-		{ id: 1, status: "Przyjete" },
-		{ id: 2, status: "Przygotowywane" },
-		{ id: 3, status: "Gotowe" },
-	]
-
-	for (const zamowienie of zamowienia) {
-		await sleep(1500)
-		yield zamowienie
-	}
-}
-
-const generatorZamowien = zamowieniaGenerator()
-;(async () => {
-	for await (const zamowienie of generatorZamowien) {
-		console.log("ID zamowienia:", zamowienie.id)
-		console.log("Status zamowienia:", zamowienie.status)
-		console.log("--------------------")
-	}
-})()
