a46ab5c77d16b6b638188958ddbdfccda2086002
Monika <codingbymon@gmail.com>
2024-07-28 19:04:19 +0200

    export/import

--- order.js
+++ order.js
@@ -7,10 +7,22 @@ const orders = [
 
 function calculateTotalOrder() {
 	let sum = 0
-	for (const order of orders) {
-		const price = order.cena
-		sum = sum + price
-	}
+	// for (const order of orders) {
+	// 	const price = order.cena
+	// 	sum = sum + price
+	// }
+	// for (let i = 0; i < orders.length; i++) {
+    //    sum += orders[i].cena
+    // }
+
+    // orders.forEach(order => {
+    //     sum+=order.cena
+    // })
+
+    // orders.map(order => {
+    //     sum+=order.cena
+    // })
+	console.log(sum)
 	return sum
 }
 
