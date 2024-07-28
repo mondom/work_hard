96864c0e23116df52017bdbb1eb1cf02c804f390
Monika <codingbymon@gmail.com>
2024-07-28 18:00:51 +0200

    export/import

---
+++ order.js
@@ -0,0 +1,10 @@
+const orders = [
+	{ produkt: "Telewizor", cena: 1500 },
+	{ produkt: "Smartfon", cena: 800 },
+	{ produkt: "Laptop", cena: 2000 },
+	{ produkt: "Słuchawki", cena: 150 },
+]
+
+function calculateTotalOrder() {}
+
+export { calculateTotalOrder }
