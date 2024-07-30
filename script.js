de19b6a2710916888a053e16475d99b1102d855b
Monika <codingbymon@gmail.com>
2024-07-30 11:13:42 +0200

    Task Pack

--- script.js
+++ script.js
@@ -1,13 +1,72 @@
 "use strict"
 
-function rollDice() {
-	const min = 1
-	const max = 6
+const celsiusValue = 25
+const fahrenheitValue = 77
+const kelvinValue = 298.15
 
-    const diceThrow = Math.floor(Math.random() * (max - min + 1)) + min
-    return diceThrow
+function celsiusToFahrenheit(celsius) {
+	const celToFahr = (celsius * 9) / 5 + 32
+	return celToFahr
 }
 
+function celsiusToKelvin(celsius) {
+	const celToKel = celsius + 273.15
+	return celToKel
+}
 
-const result = rollDice()
-console.log('Wynik rzutu kostką: ', result);
+function fahrenheitToCelsius(fahrenheit) {
+	const fahrToCel = ((fahrenheit - 32) * 5) / 9
+	return fahrToCel
+}
+
+function fahrenheitToKelvin(fahrenheit) {
+	const fahrToKel = (((fahrenheit + 459.67) * 5) / 9).toFixed(2)
+
+	return fahrToKel
+}
+
+function kelvinToCelsius(kelvin) {
+	const kelToCel = kelvin - 273.15
+	return kelToCel
+}
+
+function kelvinToFahrenheit(kelvin) {
+	const kelToFahr = ((kelvin * 9) / 5 - 459.67).toFixed(2)
+	return kelToFahr
+}
+
+console.log(
+	`${celsiusValue} stopni Celsiusza = ${celsiusToFahrenheit(
+		celsiusValue
+	)} stopni Fahrenheita`
+)
+
+console.log(
+	`${celsiusValue} stopni Celsiusza = ${celsiusToKelvin(
+		celsiusValue
+	)} stopni Kelvina`
+)
+
+console.log(
+	`${fahrenheitValue} stopni Fahrenheita = ${fahrenheitToCelsius(
+		fahrenheitValue
+	)} stopni Celsiusza`
+)
+
+console.log(
+	`${fahrenheitValue} stopni Fahrenheita = ${fahrenheitToKelvin(
+		fahrenheitValue
+	)} stopni Kelvina`
+)
+
+console.log(
+	`${kelvinValue} stopni Kelvina = ${kelvinToCelsius(
+		kelvinValue
+	)} stopni Celsiusza`
+)
+
+console.log(
+	`${kelvinValue} stopni Kelvina = ${kelvinToFahrenheit(
+		kelvinValue
+	)} stopni Fahrenheita`
+)
