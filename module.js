7dee2c9a3293b75228a75678f560736dcbcca479
Monika <codingbymon@gmail.com>
2024-07-30 09:40:37 +0200

    export/import

--- module.js
+++ module.js
@@ -1,17 +1,14 @@
-export const danie1 = {
-	nazwa: 'Pizza Margherita',
-	cena: 25.99,
-	ilosc: 2
-  };
-   
-  export const danie2 = {
-	nazwa: 'Spaghetti Bolognese',
-	cena: 18.5,
-	ilosc: 1
-  };
-   
-  export const danie3 = {
-	nazwa: 'Sałatka Cezar',
-	cena: 12.99,
-	ilosc: 3
-  };
+const listaZadan = []
+
+function dodajZadanie(tekstZadania) {
+	listaZadan.push(tekstZadania)
+}
+
+function wyswietlZadania() {
+	console.log("Lista zadań:")
+	for (let i = 0; i < listaZadan.length; i++) {
+		console.log(`${i + 1}. ${listaZadan[i]}`)
+	}
+}
+
+export { dodajZadanie, wyswietlZadania }
--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 "use strict"
 
-import * as dania from "./module.js"
+import * as zadania from './module.js'
 
-function obliczCalkowitaWartoscZamowienia(daniaArr) {
-	let sum = 0
-	for (const danie of daniaArr) {
-		const oneDish = danie.cena * danie.ilosc
-		sum += oneDish
-	}
-	return sum.toFixed(2)
-}
+zadania.dodajZadanie('Umyć naczynia')
+zadania.dodajZadanie('Zrobic zakupy');
+zadania.dodajZadanie('Napisac raport');
+zadania.dodajZadanie('Odwiedzic przyjaciela');
 
-const wszystkieDania = [dania.danie1, dania.danie2, dania.danie3]
-const calkowitaWartoscZamowienia = obliczCalkowitaWartoscZamowienia(wszystkieDania)
-
-console.log("Calkowita wartosc zamowienia: ", calkowitaWartoscZamowienia);
+zadania.wyswietlZadania()
