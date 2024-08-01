44be8dc378d1353ff9a4b5acdfcb9f5ba4abb6d5
Monika <codingbymon@gmail.com>
2024-08-01 11:56:52 +0200

    task pack

--- script.js
+++ script.js
@@ -13,8 +13,23 @@ function generateRandomNumbers() {
 	return randomNumbers
 }
 
-generateRandomNumbers()
+const randomNumbers = generateRandomNumbers()
 
-function checkResults(userNumbers, randomNumbers){
-
+function checkResults(userNumbers, randomNumbers) {
+	let matches = 0
+	for (let i = 0; i < userNumbers.length; i++) {
+		if (randomNumbers.includes(userNumbers[i])) {
+			matches++
+		}
+	}
+	return matches
 }
+
+const userNumbers = [2, 7, 15, 23, 30, 31]
+
+const result = checkResults(userNumbers, randomNumbers)
+
+console.log("Wylosowane liczby: " + randomNumbers.join(", "));
+console.log("Twoje liczby: " + userNumbers.join(", "));
+
+console.log("Liczba trafień: " + result);
